0b9ee7510761ef302748dd13e250e5a7ed323d4d
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-09 17:25:22 +0900

    Update ZOAC 4.py

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
@@ -1,5 +1,10 @@
-H,W,N,M = list(map(int, input().split(' ')))
 import math
-a = math.ceil(H / (N+1))
-b = math.ceil(W / (M+1))
-print(a*b)
+# H행 W열에서 모든 자리에 책상을 비치하며 간격은 1칸씩 띄워서 사람들이 착석 가능
+
+def solution(H,W,N,M):
+  x = math.ceil(H/(N+1))
+  y = math.ceil(W/(M+1))
+  return x*y
+
+H,W,N,M = list(map(int, input().split()))
+print(solution(H,W,N,M))
