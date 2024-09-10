7b2bae7a6ea2ba03e659fca651bf40a197b7076c
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-10 15:31:06 +0900

    Update 서울에 위치한 식당 목록 출력하기.sql

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,6 +1,6 @@
-select a.REST_ID, b.REST_NAME, b.FOOD_TYPE, b.FAVORITES, b.ADDRESS, round(avg(a.REVIEW_SCORE), 2) as SCORE
-from REST_REVIEW a
-join REST_INFO b on a.REST_ID = b.REST_ID
-group by a.REST_ID
-having b.ADDRESS like '서울%'
-order by SCORE desc, b.FAVORITES desc;
+select a.REST_ID, a.REST_NAME, a.FOOD_TYPE, a.FAVORITES, a.ADDRESS, round(avg(b.REVIEW_SCORE), 2) as SCORE
+from REST_INFO a
+join REST_REVIEW b on a.REST_ID = b.REST_ID
+group by b.REST_ID
+having a.ADDRESS like '서울%'
+order by SCORE desc, a.FAVORITES desc;
