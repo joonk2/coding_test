ffa45fac66a14cae92bf5ae98b7c192e0caf9899
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-10 12:48:50 +0900

    Update 설탕 배달.py

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,19 +1,18 @@
+# 4//5 = 몫 0
+# 4 % 5 = 나머지 4 
+# break문이 실행 or 음수가 되서 더이상 뺄게 없다면 해당 while 루프 종료
+
 sugar = int(input())
 bag = 0
 
-while sugar > 0:
+while sugar >= 0:
   if sugar % 5 == 0:
-    bag += (sugar // 5)
+    bag += (sugar//5)
     print(bag)
     break
 
   sugar -= 3
   bag += 1
 
-  if sugar == 0:
-    print(bag)
-    break
-    
-  elif sugar < 3:
-    print(-1) 
-    break
+if sugar < 0:
+  print(-1)
