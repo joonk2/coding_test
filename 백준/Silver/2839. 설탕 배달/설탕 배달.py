4473492728cc7ab9684643bb69ed2fb0a383f080
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-12 01:01:08 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,12 +1,6 @@
-# 설탕 배달 5 kg or 3 kg
-# if 18kg --> 4봉지 5kg*3 + 3kg*1
-# 설탕은 3KG 이상
-# 3KG 미만이면 -1 반환
-
-
 sugar = int(input())
 bag = 0
-while sugar >= 3:
+while sugar >= 0:
   if sugar % 5 == 0:
     bag += (sugar // 5)
     print(bag)
@@ -15,5 +9,5 @@ while sugar >= 3:
   sugar -= 3
   bag += 1
 
-if sugar < 0:
+if sugar < 0 :
   print(-1)
