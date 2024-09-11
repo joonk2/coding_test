8b894d872bc27aed8e6f9b8dead9ae41c2588cf4
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-12 00:58:55 +0900

    Update 설탕 배달.py

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,10 +1,12 @@
-# sugar % 5  --> 나머지가 0이면 가방갯수 세고 몫인 sugar // 5 만큼 가방갯수를 bag에 저장
-# sugar가 0이상일 때 까지는 5로 나눠지지 않는다면 3kg 차감을 통해 가방 갯수 가산(5의 배수가 될 때까지 반복수행)
-# sugar가 음수가 되는 순간부터 -1 반환
+# 설탕 배달 5 kg or 3 kg
+# if 18kg --> 4봉지 5kg*3 + 3kg*1
+# 설탕은 3KG 이상
+# 3KG 미만이면 -1 반환
+
 
 sugar = int(input())
 bag = 0
-while sugar >= 0:
+while sugar >= 3:
   if sugar % 5 == 0:
     bag += (sugar // 5)
     print(bag)
@@ -13,5 +15,5 @@ while sugar >= 0:
   sugar -= 3
   bag += 1
 
-if sugar < 0 :
+if sugar < 0:
   print(-1)
