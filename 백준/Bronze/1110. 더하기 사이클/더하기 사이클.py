d547c779561d74041a9c74cbee971fcee55b21e7
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-12 16:42:16 +0900

    Update 더하기 사이클.py

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,18 +1,19 @@
 # start = N
 # a = 몫
 # b = 나머지, 근데 10의 자리면 %10 하여 나머지만 가져오자
-# c = (a+b)%10
-# start = (b*10) + c
+# c = a+b
+# start = (b*10) + (c%10)
+
 
 N = int(input())
 cnt = 0
 start = N
+
 while True:
   a = start // 10
   b = start % 10
-  c = (a+b)%10
-
-  start = (b*10)+c
+  c = a+b
+  start = (b*10) + (c%10)
   cnt += 1
 
   if start == N:
