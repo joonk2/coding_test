cf483da1829ce5788d01c97a8a8c1cce07e28fd9
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-13 22:26:08 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,11 +1,10 @@
-# N회 만큼 명령을 수행한다
-# 단순반복작업으로 인해 시간 초과가 난다면 sys를 input에 저장해주자
+# N회를 입력 후에 각 기능을 입력하여 결과를 출력한다
+# input 보다 sys 처리속도가 더 빠르다
 # 저장을 위해 빈 리스트의 stack이 필요하다
-# word를 입력할 새로운 문자열 입력창도 만들자
+# word를 입력할 새로운 입력창도 만들자
 
 import sys
 input = sys.stdin.readline
-
 N = int(input())
 stack = []
 
@@ -13,20 +12,20 @@ for i in range(N):
     word = input().split()
     if word[0] == 'push':
         stack.append(word[1])
-    elif word[0] == 'top':
-        if not stack:  
-            print(-1)
-        else:
-            print(stack[-1])
-    elif word[0] == 'size':
-        print(len(stack))
-    elif word[0] == "empty":
-        if not stack:  
-            print(1)
-        else:
-            print(0)
     elif word[0] == 'pop':
-        if not stack:  
+        if not stack:
             print(-1)
         else:
             print(stack.pop(-1))
+    elif word[0] == 'size':
+        print(len(stack))
+    elif word[0] == 'empty':
+        if not stack:
+            print(1)
+        else:
+            print(0)
+    elif word[0] == 'top':
+        if not stack:
+            print(-1)
+        else:
+            print(stack[-1])
