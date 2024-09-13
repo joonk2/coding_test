bfc83bf9a3c59da3a2f17148a7e2f5470df3ce0c
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-13 22:00:01 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,30 @@
+# N회 만큼 명령을 수행한다
+# 단순반복작업으로 인해 시간 초과가 난다면 sys를 활용해보자
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+
+for i in range(N):
+    word = input().split()
+    if word[0] == 'push':
+        stack.append(word[1])
+    elif word[0] == 'top':
+        if not stack:  
+            print(-1)
+        else:
+            print(stack[-1])
+    elif word[0] == 'size':
+        print(len(stack))
+    elif word[0] == "empty":
+        if not stack:  
+            print(1)
+        else:
+            print(0)
+    elif word[0] == 'pop':
+        if not stack:  
+            print(-1)
+        else:
+            print(stack.pop(-1))
