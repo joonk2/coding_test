9944e9825272cae398e4df061f8dd4f71f6830b7
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-13 19:30:09 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,12 +1,10 @@
 # A*B*C를 한 값에서 0부터 9까지 각각의 숫자가 몇번 쓰였는지 계산한다
 
-a = int(input())
-b = int(input())
-c = int(input())
+A = int(input())
+B = int(input())
+C = int(input())
 
-100 <= a < 1000 and 100 <= b< 1000 and 100 <= c < 1000
-
-result = str(a*b*c)
-
-for i in range(0,10):
-    print(result.count(str(i)))
+100 <= A < 1000 and 100 <= B < 1000 and 100 <= C < 1000
+answer = str(A*B*C)
+for i in range(10):
+  print(answer.count(str(i)))
