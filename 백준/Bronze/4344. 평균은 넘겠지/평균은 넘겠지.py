2f84f4bce9af222d04f4631c6ad2b0c6f7765ae4
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-14 22:09:11 +0900

    Update 평균은 넘겠지.py

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,14 +1,20 @@
+# 총 테케 갯수 5
+# 과목 수 + 점수를 리스트 입력으로 받는다
+# 평균 낸 뒤에 평균 이상만 cnt하여 학생수 나눈다 --> 그걸로 % 구함
+
 import sys
 input = sys.stdin.readline
+
 T = int(input())
 
-for _ in range(T):
+for i in range(T):
     scores = list(map(int, input().split()))
     avg = sum(scores[1:]) / scores[0]
-
     cnt = 0
-    for i in scores[1:]:    
-        if i > avg:
+
+    for s in scores[1:]:
+        if s >= avg:
             cnt += 1
-    percent =  (cnt / scores[0]) * 100
-    print('%.3f' %percent + '%')
+    
+    answer = (cnt / scores[0]) * 100
+    print(f'{answer:.3f}%') 
