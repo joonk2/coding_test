92565a1108be193f05432d91dd553633828e511b
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-15 15:54:16 +0900

    Update 피보나치 함수.py

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,3 +1,16 @@
+# 생각해보자 기본 피보나치 수열 점화식 = dp[i] = dp[i-1] + dp[i-2]
+# n은 40까지니 저장을 위해 최소 41칸 만들어주자
+# 내는 바텀업 방식 사용할게 --> 그래서 모든 칸 0으로 초기화하고 만든다
+# 암튼 문제는 "Fibonacci(N) 호출했을 때, 0과 1 각각 몇 번 출력인지 구해라"다
+
+#              Fibo(3)
+#             /      \
+#       Fibo(2)     Fibo(1)
+#         /    \
+#   Fibo(1)   Fibo(0)
+
+# Fibonacci = 3일때는 F(0) 1번,   F(1) 2번 출력하네
+
 import sys
 input = sys.stdin.readline
 T = int(input())
