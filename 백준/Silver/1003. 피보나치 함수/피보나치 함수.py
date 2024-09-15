b8af28fff8d470b661bbd7592e9da780cb9625f4
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-15 15:53:17 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -5,16 +5,17 @@ import sys
 input = sys.stdin.readline
 T = int(input())
 
-for tc in range(T): # 테스트 케이스 개수만큼 반복
-    n = int(input()) 
+for i in range(T):
+    dp_0 = [0] * 41
+    dp_1 = [0] * 41
 
-    dp_0 = [0] * 41 # 0이 호출되는 횟수를 저장할 list
-    dp_1 = [0] * 41 # 1이 호출되는 횟수를 저장할 list
-    dp_0[0] = 1     # fibonacci(0)을 호출했을 때 0이 호출되는 횟수 1
-    dp_1[1] = 1     # fibonacci(1)을 호출했을 때 1이 호출되는 횟수 1
+    dp_0[0] = 1
+    dp_1[1] = 1
 
-    for i in range(2, n + 1): 
-        dp_0[i] = dp_0[i - 1] + dp_0[i - 2]
-        dp_1[i] = dp_1[i - 1] + dp_1[i - 2]
+    n = int(input())
+
+    for i in range(2, n+1):
+        dp_0[i] = dp_0[i-1] + dp_0[i-2]
+        dp_1[i] = dp_1[i-1] + dp_1[i-2]
 
     print(dp_0[n], dp_1[n])
