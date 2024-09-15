f55dce29210f2624ae299707231095c132546ed0
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-15 15:53:55 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,6 +1,3 @@
-# 생각해보자 기본 피보나치 수열 점화식 = dp[i] = dp[i-1] + dp[i-2]
-# n은 40까지니 저장을 위해 최소 41칸 만들어주자
-
 import sys
 input = sys.stdin.readline
 T = int(input())
@@ -8,14 +5,13 @@ T = int(input())
 for i in range(T):
     dp_0 = [0] * 41
     dp_1 = [0] * 41
-
     dp_0[0] = 1
     dp_1[1] = 1
-
+    
     n = int(input())
 
-    for i in range(2, n+1):
-        dp_0[i] = dp_0[i-1] + dp_0[i-2]
-        dp_1[i] = dp_1[i-1] + dp_1[i-2]
-
+    for j in range(2, n+1):
+        dp_0[j] = dp_0[j-1] + dp_0[j-2]
+        dp_1[j] = dp_1[j-1] + dp_1[j-2]
+    
     print(dp_0[n], dp_1[n])
