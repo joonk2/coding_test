744329dd58a897f6272eff4401e4cc88475b030c
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-19 15:04:06 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+if N == 1:
+    print(1)
+else:
+    dp = [0] * (N+1)
+    dp[1] = 1
+    dp[2] = 1
+    for i in range(3, N+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    print(dp[N])
