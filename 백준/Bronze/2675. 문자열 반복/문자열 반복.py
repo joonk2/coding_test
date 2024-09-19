65b3e13c99f3cd1f5028dc6e5147ff35e4b86af4
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-19 14:36:00 +0900

    [Bronze II] Title: 문자열 반복, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,12 +1,10 @@
 import sys
 input = sys.stdin.readline
-
 T = int(input())
 
 for i in range(T):
     answer = ''
     cnt, word = input().split()
-
     for j in word:
         answer += int(cnt) * j
     print(answer)
