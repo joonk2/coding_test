a60c5f2ed0cf5409a25032c37801430083e14738
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-19 15:41:32 +0900

    [Gold II] Title: 피보나치 수 3, Time: 412 ms, Memory: 89936 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,22 @@
+# 값이 너무 크면 메모리 초과다 ㅇㅋ?
+
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+
+mod = 1000000
+pisano_period = 1500000
+n %= pisano_period
+
+if n == 0:
+    print(0)
+else:
+    dp = [0] * (n+1)
+    dp[0] = 0
+    dp[1] = 1
+
+    for i in range(2, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % mod
+    
+    print(dp[n])
