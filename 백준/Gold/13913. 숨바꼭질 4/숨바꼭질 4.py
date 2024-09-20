edda27388d0736c251bc11fa992fd755775bdf44
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-20 22:34:27 +0900

    Update 숨바꼭질 4.py

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -23,7 +23,7 @@ def bfs(s, e):
                     v[next] = v[c]+1
                     shortcut[next] = c # 경로 역추적을 위한 빌드업(직전 위치 최신화)
                     
-    return '다시 해라 임마' # 디버깅
+    return '다시해라 임마' # 디버깅
 
 
 
@@ -32,7 +32,7 @@ def upsidedown_path(s, e, shortcut):
     c = e # 동생의 위치인 종착점(e)에서부터 출발
     while c != s: # 동생의 위치에서 수빈의 위치까지 즉 출발점(s)까지 역으로 추적
         way.append(c) # 현재 위치를 저장 --> 동생의 위치부터 수빈의 위치까지 역순으로 경로 저장
-        c = shortcut[c] # 17 -> 16 -> 8 -> 4
+        c = shortcut[c] # [4] [3] [2] [1]  17 -> 16 -> 8 -> 4
     
     way.append(s) # 이제 c == s일때 마지막으로 수빈의 시작점 5 추가
 
