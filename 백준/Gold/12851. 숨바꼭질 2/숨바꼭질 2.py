9e790f34665e54567c09d441d25d0262470c5641
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-20 13:37:28 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 276 ms, Memory: 37624 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -5,7 +5,6 @@
 # 5. 현재좌표와 끝 좌표가 같을 때 시간 = 현재좌표-1, 최단거리 가산, 그리고 계속
 
 
-
 from collections import deque
 
 def bfs(s, e):
@@ -13,10 +12,9 @@ def bfs(s, e):
     v = [0] * 100001
     q.append(s)
     v[s] = 1
-    
+
     min_time = 0
     cnt_shortcut = 0
-    
 
     while q:
         c = q.popleft()
@@ -24,21 +22,18 @@ def bfs(s, e):
             min_time = v[c]-1
             cnt_shortcut += 1
             continue
-        
+
         for next in (c-1, c+1, c*2):
             if 0 <= next <= 100000:
-
-                # 처음 방문시
                 if v[next] == 0:
                     q.append(next)
-                    v[next] = v[c] + 1
-
-                # 이미 방문했지만 다른 루트를 통해서도 같은 시간이 걸려서 도달했을 때
+                    v[next] = v[c]+1
                 elif v[next] == v[c] + 1:
                     q.append(next)
 
     return min_time, cnt_shortcut
 
+    
 
 
 N, K = map(int, input().split())
