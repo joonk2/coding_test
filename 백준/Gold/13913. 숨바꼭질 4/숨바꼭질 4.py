0f7773b3ce28274d792d1b57b68838a9346ae672
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-21 01:37:32 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 108 ms, Memory: 47664 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -4,7 +4,6 @@ def bfs(s, e):
     q = deque([])
     v = [0] * 100001
     shortcut = [0] * 100001
-
     q.append(s)
     v[s] = 1
 
@@ -21,26 +20,24 @@ def bfs(s, e):
                 if v[next] == 0:
                     q.append(next)
                     v[next] = v[c]+1
-                    shortcut[next] = c # 경로 역추적을 위한 빌드업(직전 위치 기록)
-                    
-    return '다시해라 임마' # 디버깅
+                    shortcut[next] = c
 
+    return '다시해라 임마' # 디버깅
 
 
 def upsidedown_path(s, e, shortcut):
     way = []
-    c = e # 동생의 위치인 종착점(e)에서부터 출발
-    while c != s: # 동생의 위치에서 수빈의 위치까지 즉 출발점(s)까지 역으로 추적
-        way.append(c) # 현재 위치를 저장(17 출발) --> 동생의 위치부터 수빈의 위치까지 역순으로 경로 저장
-        c = shortcut[c]  # 인덱스 위치관계 참고 (1개씩 줄어드네) -> a[17] a[16] a[8] a[4]  16 -> 15-> 8 -> 4
-    
-    way.append(s) # 이제 c == s일때 마지막으로 수빈의 시작점 5 추가
+    c = e
+    while c != s:
+        way.append(c)
+        c = shortcut[c] # 내림차순 역으로
+    way.append(s)
 
     return way[::-1]
 
 N, K = map(int, input().split())
-min_time, shortcut= bfs(N, K)
-print(min_time)
+min_time, shortcut = bfs(N, K)
 
+print(min_time)
 final_shortcut = upsidedown_path(N, K, shortcut)
 print(' '.join(map(str, final_shortcut)))
