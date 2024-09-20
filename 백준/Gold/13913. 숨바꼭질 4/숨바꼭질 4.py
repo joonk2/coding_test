3227c56260e048ac7d6a72990c7acfda5e5f76a2
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-20 16:31:26 +0900

    Update 숨바꼭질 4.py

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -8,33 +8,40 @@ def bfs(s, e):
     q.append(s)
     v[s] = 1
 
+    min_time = 0
+
     while q:
         c = q.popleft()
-        if c == e: 
-            return v[c]-1, shortcut
+        if c == e:
+            min_time = v[c]-1
+            return min_time, shortcut
         
         for next in (c-1, c+1, c*2):
             if 0 <= next <= 100000:
                 if v[next] == 0:
                     q.append(next)
-                    v[next] = v[c] + 1
+                    v[next] = v[c]+1
                     shortcut[next] = c # 방문 안했을때는 현재위치c가 제일 최신위치다
 
-def recursive_path(s, e, shortcut):
-    way = []
+                elif v[next] == v[c]+1:
+                    q.append(next)
+
+def upsidedown_path(s, e, shortcut):
     c = e # 동생의 위치인 종착점(e)에서부터 출발
+    way = []
     while c != s: # 동생의 위치에서 수빈의 위치까지 즉 출발점(s)까지 역으로 추적
         way.append(c) # 현재 위치를 저장 --> 동생의 위치부터 수빈의 위치까지 역순으로 경로 저장
         c = shortcut[c] # 17 -> 16 -> 8 -> 4 순으로 저장된다
-
+    
     way.append(s) # 이제 c == s일때 마지막으로 수빈의 시작점 5 추가
 
     return way[::-1]
+
 
 
 N, K = map(int, input().split())
 min_time, shortcut = bfs(N, K)
 print(min_time)
 
-final_shortcut = recursive_path(N, K, shortcut)
+final_shortcut = upsidedown_path(N, K, shortcut)
 print(' '.join(map(str, final_shortcut)))
