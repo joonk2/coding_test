295573fc93610847d8f60f66b3753e74f8e58685
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-20 15:36:15 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 108 ms, Memory: 48072 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -2,8 +2,6 @@ from collections import deque
 
 def bfs(s, e):
     q = deque([])
-    min_time = 0
-
     v = [0] * 100001
     shortcut = [0] * 100001
 
@@ -12,30 +10,29 @@ def bfs(s, e):
 
     while q:
         c = q.popleft()
-        if c == e:
+        if c == e: 
             return v[c]-1, shortcut
         
         for next in (c-1, c+1, c*2):
             if 0 <= next <= 100000:
-                if v[next] ==  0:
+                if v[next] == 0:
                     q.append(next)
                     v[next] = v[c] + 1
-                    shortcut[next] = c
+                    shortcut[next] = c # 방문 안했을때는 현재위치c가 제일 최신위치다
 
 def recursive_path(s, e, shortcut):
     way = []
-    c = e
-    while c != s:
-        way.append(c)
-        c = shortcut[c]
-    way.append(s)
+    c = e # 동생의 위치인 종착점(e)에서부터 출발
+    while c != s: # 동생의 위치에서 수빈의 위치까지 즉 출발점(s)까지 역으로 추적
+        way.append(c) # 현재 위치를 저장 --> 동생의 위치부터 수빈의 위치까지 역순으로 경로 저장
+        c = shortcut[c] # 17 -> 16 -> 8 -> 4 순으로 저장된다
+
+    way.append(s) # 이제 c == s일때 마지막으로 수빈의 시작점 5 추가
 
     return way[::-1]
 
 
-
 N, K = map(int, input().split())
-
 min_time, shortcut = bfs(N, K)
 print(min_time)
 
