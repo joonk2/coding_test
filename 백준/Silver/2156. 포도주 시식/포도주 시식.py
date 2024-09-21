a688131c84c0da54454c517b382ea7bfeb1bcae5
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-21 22:36:04 +0900

    [Silver I] Title: 포도주 시식, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
 wine = [0] * 10001
 
@@ -7,12 +8,14 @@ for i in range(1, n+1):
     wine[i] = int(input())
 
 dp = [0] * 10001
+
 dp[1] = wine[1]
 dp[2] = wine[1] + wine[2]
-dp[3] = max(wine[1]+wine[3], wine[2]+wine[3], wine[1]+wine[2])
+dp[3] = max(dp[2], wine[1]+wine[3], wine[2]+wine[3])
 
 for j in range(4, n+1):
-    dp[j] = max(dp[j-3] + wine[j-1] + wine[j], dp[j-2]+wine[j], dp[j-1])
+    dp[j] = max(dp[j-3] + wine[j-1] + wine[j], dp[j-2] + wine[j], dp[j-1])
+    # j=4, 5 대입하면 코드가 100% 이해될 것이야
 
 
 print(dp[n])
