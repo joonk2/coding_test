fd1944a1b5175b432ce32928528a74920b3a43db
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-22 02:14:45 +0900

    Update 연속합.py

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -2,19 +2,22 @@
 # [0] 10 -4 3 1 5 6 -35 12 21 -1
 # 위는 [0]을 추가하여 인덱스 0 ~ 10인데 범위로 인덱스 1, n+1까지 조정가능
 
+# max(seq[i], dp[i-1]+seq[i]) --> dp[1:] = [10, 6, 9, 10, 15, 21, -14, 12, 33, 32]
+
+
 import sys
 
 input = sys.stdin.readline
-N = int(input())
-seq = [0] + list(map(int, input().split()))  # 1부터 시작하도록 0을 추가
+n = int(input())
+seq = [0] + list(map(int, input().split())) # 1부터 시작하도록 0을 추가
+dp = [0] * (n+1) 
+dp[1] = seq[1]
 
 if max(seq[1:]) <= 0:
-    ans = max(seq[1:])  # 음수만 있을 때 최대값
+    print(max(seq[1:])) # 음수만 있을 때 최대값
 else:
-    dp = [0] * (N + 1)  # N+1로 초기화
-    dp[1] = seq[1]  
-    for i in range(2, N + 1):
-        dp[i] = max(seq[i], dp[i - 1] + seq[i])  # 연속합 계산
+    for i in range(2, n+1):
+        dp[i] = max(seq[i], dp[i-1]+seq[i]) # 연속합 계산
     ans = max(dp[1:])
 
-print(ans)
+    print(ans) 
