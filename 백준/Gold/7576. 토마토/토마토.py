259317395159359309230bbcc9c7a9a605051c32
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-24 22:21:34 +0900

    Update 토마토.py

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -39,39 +39,41 @@
 
 
 
-# bfs 함수       x, y  행 열    nx n   ny m
+# bfs 함수        x, y  행 열    nx n   ny m
 from collections import deque
 
 def bfs():
-    v = [ [0] * m for _ in range(n)]
     q = deque([])
+    v = [ [0] * m for _ in range(n)]
 
-    # 첫 노드
+    # 첫 노드 입력
     for i in range(n):
         for j in range(m):
             if graph[i][j] == 1:
-                q.append((i,j))
+                q.append((i, j))
+    
 
     while q:
-        x, y = q.popleft()
+        cx, cy = q.popleft()
 
-        # 4방향
+        # 방향
         dx = [-1,1,0,0]
         dy = [0,0,-1,1]
-        
+
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+            nx = cx + dx[i]
+            ny = cy + dy[i]
 
             # x, y 행 열
             if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-                q.append((nx, ny))
-                graph[nx][ny] = graph[x][y] + 1
-                v[nx][ny] = 1
+                q.append((nx,ny))
+                graph[nx][ny] = graph[cx][cy] + 1
 
 
 
 
-# 1. 첫줄 입력(열, 행), 둘째줄 입력, bfs함수 실행
+
+# 1. 첫줄 입력, 둘째줄 입력, bfs함수 실행
 m, n = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 bfs()
@@ -79,7 +81,6 @@ bfs()
 # 2. 결과 출력
 # 만약, 저장될 때부터 모든 토마토가 익어있는 상태이면 0을 출력해야 하고,
 # 토마토가 모두 익지는 못하는 상황이면 -1을 출력
-
 result = 0
 for i in range(n):
     for j in range(m):
@@ -88,4 +89,4 @@ for i in range(n):
             exit(0)
         result = max(result, graph[i][j])
 
-print(result-1)
+print(result - 1)
