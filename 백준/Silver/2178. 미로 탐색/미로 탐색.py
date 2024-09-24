8c1b2af76d03ac9355bd2d7215052dae70945891
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-24 21:01:23 +0900

    Update 미로 탐색.py

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,25 +3,25 @@
 # import sys
 # sys.stdin = open("input.txt", "r")
 
-def bfs(start_i, start_j, end_i, end_j):
-    queue = []
-    visited = [[0] * M for _ in range(N)]
+def bfs(si, sj, ei, ej):
+    q = []
+    v = [[0] * M for _ in range(N)]
 
-    queue.append((start_i, start_j))
-    visited[start_i][start_j] = 1 # 방문 표시
+    q.append((si, sj))
+    v[si][sj] = 1 # 방문 표시
 
-    while queue:
-        current_i, current_j = queue.pop(0)
-        if (current_i, current_j) == (end_i, end_j): # 현재가 목적지 좌표라면
-            return visited[end_i][end_j]
+    while q:
+        ci, cj = queue.pop(0)
+        if (ci, cj) == (ei, ej): # 현재가 목적지 좌표라면
+            return v[ei][ej]
 
         # 4방향 범위내, 조건에 맞으면 queue에 삽입
-        for direction_i, direction_j in ((-1,0), (1,0), (0,-1), (0,1)): #상하좌우
-            next_i, next_j = current_i + direction_i, current_j + direction_j
-            if 0 <= next_i < N and  0 <= next_j < M and arr[next_i][next_j] == 1 and visited[next_i][next_j] == 0:
+        for di, dj in ((-1,0), (1,0), (0,-1), (0,1)): #상하좌우
+            ni, nj = ci + di, cj + dj
+            if 0 <= ni < N and  0 <= nj < M and arr[ni][nj] == 1 and v[ni][nj] == 0:
                 # 만약 다음 좌표의 행과 열이 입력한 범위 내에 있고, 길이 있으며(=1), 방문하지 않았는 곳이라면(=0)
-                queue.append((next_i, next_j))
-                visited[next_i][next_j] = visited[current_i][current_j]+1
+                q.append((ni, nj))
+                v[ni][nj] = v[ci][cj]+1
 
 N,M = map(int, input().split())
 arr = [list(map(int, input())) for _ in range(N)]
