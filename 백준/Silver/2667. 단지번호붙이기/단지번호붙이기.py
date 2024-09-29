9a32ae7eb330a2a374a0403208a3f369cba6f1c0
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-29 21:51:28 +0900

    [Silver I] Title: 단지번호붙이기, Time: 48 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,40 +1,39 @@
 # 2. bfs
-
 from collections import deque
 
 def bfs(si, sj):
     q = deque([])
-    q.append((si,sj))
+    q.append((si, sj))
     v[si][sj] = 1
     cnt = 1
 
     while q:
-        ci,cj = q.popleft()
-        
-        for di, dj in((-1,0), (1,0), (0,-1), (0,1)):
+        ci, cj = q.popleft()
+
+        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             ni = ci+di
             nj = cj+dj
-            if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == 0 and arr[ni][nj] == 1:
+
+            if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 1 and v[ni][nj] == 0:
                 q.append((ni, nj))
                 v[ni][nj] = 1
                 cnt += 1
-
     return cnt
 
 
 
+
 # 1. 입출력을 위한 세팅
-  
 N = int(input())
+
 arr = [list(map(int, input())) for _ in range(N)]
-v = [ [0]*N for _ in range(N) ]
+v = [ [0] * N for _ in range(N)]
 ans = []
 
 for i in range(N):
     for j in range(N):
-        # 방문하지 않은 아파트 발견시 bfs
         if arr[i][j] == 1 and v[i][j] == 0:
-            ans.append(bfs(i,j))
+            ans.append(bfs(i, j))
 
 ans.sort()
 print(len(ans), *ans, sep = '\n')
