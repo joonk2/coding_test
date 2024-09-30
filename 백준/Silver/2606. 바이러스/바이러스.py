4e645c6dbaa3ce81266c652ebadf5acb49dbd480
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-30 13:54:32 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -2,30 +2,35 @@
 
 
 # 2. dfs
-
 def dfs(c):
     global ans
     ans += 1
-    
+
     v[c] = 1
 
-    for n in adj[c]: 
+    for n in adj[c]:
         if not v[n]:
             dfs(n)
 
 
 
+
 # 1. 입출력 세팅
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 M = int(input())
 
-adj = [ [] for _ in range(N+1)]
+# 늘 그렇지만 우린 인덱스 1번부터 쓴다 0은 버린다
+adj = [ [] for _ in range(N+1) ]
 for _ in range(M):
     s, e = map(int, input().split())
     adj[s].append(e)
     adj[e].append(s)
 
+
 ans = 0
-v = [0]*(N+1)
+v = [0]* (N+1)
 dfs(1)
-print(ans-1)
+print(ans - 1)
