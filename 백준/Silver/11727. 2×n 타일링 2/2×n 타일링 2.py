4f2645010cb74230762e339a0e312d9847799a66
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-30 18:25:46 +0900

    Update 2×n 타일링 2.py

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -18,5 +18,5 @@ print(dp[n] % 10007)
 # dp = [0] * (n+1)
 # dp[0], dp[1] = 1, 1
 # for i in range(2, n+1):
-#     dp[i] = (dp[i-1] + dp[i-2]*2) % 10007
+#     dp[i] = (dp[i-2]*2 + dp[i-1]) % 10007
 # print(dp[n])
