7488aec7aa9c69e57089a7522b83e491562e51fe
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-30 19:51:34 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,12 +1,15 @@
 import sys
-
 input = sys.stdin.readline
+
 n = int(input())
 dp = [0] * (n+1)
 dp[0] = 0
 dp[1] = 1
 
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+def fibonacci(n):
+    for i in range(2, n+1):
+        dp[i] = dp[i-2] + dp[i-1]
 
-print(dp[n])
+    return dp[n]
+
+print(fibonacci(n))
