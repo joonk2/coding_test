3f3e4ea4dcc284bbbfc3bd49a40764f9cecb509f
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-30 14:47:39 +0900

    [Silver III] Title: 바이러스, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -12,9 +12,6 @@ def dfs(c):
         if not v[n]:
             dfs(n)
 
-
-
-
 # 1. 입출력 세팅
 import sys
 input = sys.stdin.readline
@@ -22,15 +19,18 @@ input = sys.stdin.readline
 N = int(input())
 M = int(input())
 
-# 늘 그렇지만 우린 인덱스 1번부터 쓴다 0은 버린다
-adj = [ [] for _ in range(N+1) ]
+
+
+# 컴퓨터 세팅 --> 인덱스 0~7, 근데 실제로는 1~7만 필요
+adj = [ [] for _ in range(N+1)]
+
+# adjacency list connecting
 for _ in range(M):
     s, e = map(int, input().split())
     adj[s].append(e)
     adj[e].append(s)
 
-
 ans = 0
-v = [0]* (N+1)
+v = [0] * (N+1)
 dfs(1)
-print(ans - 1)
+print(ans-1)
