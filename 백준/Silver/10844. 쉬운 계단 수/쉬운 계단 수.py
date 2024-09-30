23e7a8e7d3ef89789a53bbfed73eb7c43b7716df
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-09-30 11:34:07 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,20 +1,8 @@
-# dp[2][0] = dp[1][1]: 10
-# dp[2][1] = dp[1][0] + dp[1][2]: 12, 21
-# dp[2][2] = dp[1][1] + dp[1][3]: 23, 32
-# dp[2][3] = dp[1][2] + dp[1][4]: 34, 43
-# dp[2][4] = dp[1][3] + dp[1][5]: 45, 54
-# dp[2][5] = dp[1][4] + dp[1][6]: 56, 65
-# dp[2][6] = dp[1][5] + dp[1][7]: 67, 76
-# dp[2][7] = dp[1][6] + dp[1][8]: 78, 87
-# dp[2][8] = dp[1][7] + dp[1][9]: 89, 98
-# dp[2][9] = dp[1][8]: 9
-
-
-import sys
+import sys 
 input = sys.stdin.readline
 N = int(input())
 
-dp = [ [0]*10 for _ in range(N+1) ]
+dp = [ [0]*10 for _ in range(N+1)]
 
 for i in range(1, 10):
     dp[1][i] = 1
@@ -23,11 +11,9 @@ for i in range(2, N+1):
     for j in range(10):
         if j == 0:
             dp[i][j] = dp[i-1][1]
-
         elif j == 9:
             dp[i][j] = dp[i-1][8]
-
         else:
             dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
 
-print(sum(dp[N]) % 1000000000 )
+print(sum(dp[N]) % 1000000000)
