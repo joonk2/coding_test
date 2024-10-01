ff42a33981c956efbc9a7ff5bd75a88c2f497783
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-02 00:42:34 +0900

    [Silver III] Title: 특별상이라도 받고 싶어, Time: 1152 ms, Memory: 83900 KB -BaekjoonHub

--- 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.py
+++ 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.py
@@ -1,16 +1,19 @@
 import sys
 # sys.setrecursionlimit(10**6)
 N = int(sys.stdin.readline())
-array = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
-def recursion(array, N):
-    if len(array) == 1: # 특별상을 받을 수 있는 사람이 한 명이라면, 그 사람이 뽑힌다.
-        return array[0][0]
-    top_left = [i[:N//2] for i in array[:N//2]]
-    top_right = [i[N//2:] for i in array[:N//2]]
-    bottom_left = [i[:N//2] for i in array[N//2:]]
-    bottom_right = [i[N//2:] for i in array[N//2:]]
-    temp = [recursion(top_left, N // 2), recursion(top_right, N // 2),
-            recursion(bottom_left, N // 2), recursion(bottom_right, N // 2)]
+arr = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
+
+def recursion(arr, N):
+    if len(arr) == 1: # 후보자가 한명 밖에 없으면 그냥 맨 첫 번째 사람 뽑는다
+        return arr[0][0]
+    top_left = [i[:N//2] for i in arr[:N//2]]
+    top_right = [i[N//2:] for i in arr[:N//2]]
+    bottom_left = [i[:N//2] for i in arr[N//2:]]
+    bottom_right = [i[N//2:] for i in arr[N//2:]]
+    temp = [recursion(top_left, N//2), recursion(top_right, N//2),
+            recursion(bottom_left, N//2), recursion(bottom_right, N//2)]
     temp.sort()
+
     return temp[1]
-print(recursion(array, N))
+
+print(recursion(arr, N))
