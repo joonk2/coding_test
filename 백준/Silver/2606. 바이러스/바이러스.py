e39f43fe4ecd216d63f9e2564a2e0510bb3599f0
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-01 17:06:00 +0900

    Update 바이러스.py

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,4 +1,7 @@
-# 갯수 셀때는 dfs가 답이여
+# 1번 컴터랑 연결된 컴터들이 바이러스에 걸려서 총4대 도루묵
+# 컴퓨터 갯수에 따른 위치 배치 --> 우린 보통 인덱스 1~7을 사용하지 인덱스 0은 버리걸랑
+# 연결된 링크는 6개  (출발 s -> 끝 e      끝 e -> 출발 s)
+# 연결된 링크 --> 마치 인접행렬?
 
 
 # 2. dfs
@@ -12,25 +15,22 @@ def dfs(c):
         if not v[n]:
             dfs(n)
 
-# 1. 입출력 세팅
+# 1.입출력 셋팅
 import sys
 input = sys.stdin.readline
-
-N = int(input())
+N= int(input())
 M = int(input())
-
-
 
 # 컴퓨터 세팅 --> 인덱스 0~7, 근데 실제로는 1~7만 필요
 adj = [ [] for _ in range(N+1)]
+v = [0] * (N+1)
 
-# adjacency list connecting
 for _ in range(M):
-    s, e = map(int, input().split())
+    s, e = list(map(int, input().split()))
     adj[s].append(e)
     adj[e].append(s)
 
+
 ans = 0
-v = [0] * (N+1)
 dfs(1)
 print(ans-1)
