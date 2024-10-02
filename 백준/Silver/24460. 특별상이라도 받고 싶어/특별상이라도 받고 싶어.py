c86f43794222c7f42f17fb284fba3b368505c7c6
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-02 11:05:24 +0900

    Update 특별상이라도 받고 싶어.py

--- 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.py
+++ 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.py
@@ -6,6 +6,8 @@ arr = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
 def recursion(arr, N):
     if len(arr) == 1: # 후보자가 한명 밖에 없으면 그냥 맨 첫 번째 사람 뽑는다
         return arr[0][0]
+
+    # 그게 아니라면 가로로 2등분 하고 거기서 또 세로로 반 나눠서 4등분하여 재귀
     top_left = [i[:N//2] for i in arr[:N//2]]
     top_right = [i[N//2:] for i in arr[:N//2]]
     bottom_left = [i[:N//2] for i in arr[N//2:]]
