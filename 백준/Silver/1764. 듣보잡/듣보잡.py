e5e0311f435bdb59b26d8f3461f90b1328e1b114
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-02 23:42:09 +0900

    [Silver IV] Title: 듣보잡, Time: 2364 ms, Memory: 43924 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,6 +1,6 @@
-# 입출력 예제를 보면 알 수 있는데 듣지도 보지도 못한 놈 중복되면 당첨이다 이말이다
-
-N, M = list(map(int, input().strip().split()))
+# 1. 입출력 예제를 보면 알 수 있는데 듣지도 보지도 못한 놈 중복되면 당첨이다 이말이다
+# 2. 듣보잡의 수와 그 명단을 사전순으로 출력한다.
+N, M = list(map(int, input().split()))
 
 a = set()
 for i in range(N):
@@ -10,7 +10,8 @@ b = set()
 for j in range(M):
     b.add(input())
 
-res = sorted(list(a & b))  # 같은 말   $$a \cap b$$
+res = sorted(list(a&b))  # 같은말이다  $$ a \cap b $$
+
 print(len(res))
 for r in res:
     print(r)
