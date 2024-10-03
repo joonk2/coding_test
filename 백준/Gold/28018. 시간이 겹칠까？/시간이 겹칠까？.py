3a593a92f0a004156d1f65c3caf04c8bd22dbe33
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-03 16:48:18 +0900

    [Gold V] Title: 시간이 겹칠까?, Time: 396 ms, Memory: 81596 KB -BaekjoonHub

--- 백준/Gold/28018. 시간이 겹칠까？/시간이 겹칠까？.py
+++ 백준/Gold/28018. 시간이 겹칠까？/시간이 겹칠까？.py
@@ -8,23 +8,37 @@
 # 입출력 2
 # s=1 e=5    s=3 e=6
 # [0,1,0,1,0,0,-1,-1] --> [0,1,1,2,2,2,1,0]
-# 3,4,5시는 시간 2번 겹치고 6시부터는 1번 그리고 7시부터 예약 가능
+# 3,4,5는 시간 2번 겹치고 6시부터는 1번 그리고 7시부터 예약 가능
 
+
+# 누적합과 cnt로 보자
+
+# 입출력 1
+# s=1, e=3 초기에는 1대입 맨 끝+1에는 -1 대입해서
+# --> 이후의 리스트를 보자 즉 4시부터는 작업이 없다
+# [0,1,0,0,-1] --> [0,1,1,1,0]
+
+# 입출력 2
+# s=1, e=5,     s=3, e=6
+# [0,1,0,1,0,0,-1,-1] --> [0,1,1,2,2,2,1,0]
+# 3,4,5시는 시간 2번씩 겹치고 6시부터 1번 그리고 7시부터 예약가능
+
+import sys
+input = sys.stdin.readline
 N = int(input())
+
 time = [0] * 1000002
 
-# 1. 누적합
 for i in range(N):
-    s, e = list(map(int, input().split()))
+    s, e = list(map(int, input().strip().split()))
     time[s] += 1
     time[e+1] += -1
 
 for j in range(1, len(time)):
     time[j] += time[j-1]
 
-# 2. cnt
-cnt_hour = int(input())
-hour_list = list(map(int, input().split()))
+Q = int(input())
+hour_list = list(map(int, input().strip().split()))
 
-for k in range(cnt_hour):
+for k in range(Q):
     print(time[hour_list[k]])
