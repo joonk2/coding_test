9e6a03e2ce6c6e230d4054aba0e340abd0aca814
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-03 15:32:21 +0900

    Update 시간이 겹칠까？.py

--- 백준/Gold/28018. 시간이 겹칠까？/시간이 겹칠까？.py
+++ 백준/Gold/28018. 시간이 겹칠까？/시간이 겹칠까？.py
@@ -10,22 +10,19 @@
 # [0,1,0,1,0,0,-1,-1] --> [0,1,1,2,2,2,1,0]
 # 3,4,5는 시간 2번 겹치고 6시부터는 1번 그리고 7시부터 예약 가능
 
-
 N = int(input())
-
-time = [0]*1000002
+time = [0] * 1000002
 
 for i in range(N):
-    s, e = map(int, input().split())
+    s, e = list(map(int, input().split()))
     time[s] += 1
     time[e+1] += -1
-    
-for i in range(1, len(time)):
-    time[i] += time[i-1]
 
-count_hour = int(input())
+for j in range(1, len(time)):
+    time[j] += time[j-1]
 
+cnt_hour = int(input())
 hour_list = list(map(int, input().split()))
 
-for i in range(count_hour):
-    print(time[hour_list[i]])
+for k in range(cnt_hour):
+    print(time[hour_list[k]])
