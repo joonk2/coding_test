baa5e35b9b72088b88982fd425ab0559b3d2aeb8
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-05 14:27:13 +0900

    [Silver IV] Title: ATM, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,17 +1,11 @@
-# 최솟값의 합 but 각 자리별 누적 합 --> 정렬(asc)
-# 1 2 3 3 4 --> 1 + 3 + 6 + 9 + 13 --> 32
-# dp는 출력초과
-
 import sys
 input = sys.stdin.readline
 N = int(input())
-Pi = [0] + list(map(int, input().strip().split()))
+arr = [0] + list(map(int, input().strip().split()))
+arr.sort()
 
-min_sum = 0
-Pi.sort()
-# 각 자리별 누적합 알고리즘
+sum = 0
 for i in range(1, N+1):
     for j in range(i+1):
-        min_sum += Pi[j]
-
-print(min_sum)
+        sum += arr[j]
+print(sum)
