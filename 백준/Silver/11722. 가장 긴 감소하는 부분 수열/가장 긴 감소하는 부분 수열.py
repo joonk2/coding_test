1afae2c2c3580c4a2767f6704e68fe3e2ea3a9e6
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-05 11:50:57 +0900

    Update 가장 긴 감소하는 부분 수열.py

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -7,7 +7,7 @@ dp = [1] * (N+1)
 
 for i in range(1, N+1):
     for j in range(i):
-        if arr[i] < arr[j]:
+        if arr[j] > arr[i]:
             dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp[1:]))
