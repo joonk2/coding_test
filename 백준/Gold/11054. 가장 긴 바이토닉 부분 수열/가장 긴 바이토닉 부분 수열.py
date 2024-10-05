0be769973174eff95d6ba66c72c5d666d9180468
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-05 13:14:59 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 244 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [0] + list(map(int, input().strip().split()))
+increase_dp = [1] * (N+1)
+decrease_dp = [1] * (N+1)
+
+# 1. 증가 수열
+for i in range(1, N+1):
+    for j in range(1, i):
+        if arr[i] > arr[j]:
+            increase_dp[i] = max(increase_dp[i], increase_dp[j]+1)
+
+
+# 2. 감소 수열
+for i in range(N, 0, -1):
+    for j in range(i+1, N+1):
+        if arr[i] > arr[j]:
+            decrease_dp[i] = max(decrease_dp[i], decrease_dp[j]+1)
+
+
+# 3. 출력
+res = [0] * (N+1)
+for r in range(1, N+1):
+    res[r] = increase_dp[r] + decrease_dp[r]-1
+print(max(res))
