d380a9a5c4705195e4b03aa9aa128edc415cc689
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-06 23:49:06 +0900

    [Silver III] Title: 소수 구하기, Time: 4876 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+M, N = list(map(int, input().strip().split()))
+for i in range(M, N+1):
+    if i == 1:
+        continue
+    for j in range(2, int(i**0.5)+1):
+        if i % j == 0:
+            break
+    else:
+        print(i)
