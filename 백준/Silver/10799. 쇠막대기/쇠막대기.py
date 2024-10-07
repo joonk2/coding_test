13b08c1c7a7ee9d4635b4e990a447286be3a7509
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-07 18:26:15 +0900

    [Silver II] Title: 쇠막대기, Time: 64 ms, Memory: 31552 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+s = input()
+stack = []
+cnt = 0
+
+for i in range(len(s)):
+    if s[i] == '(':
+        stack.append(s[i])
+    elif s[i] == ')':
+        if s[i-1] == '(':
+            cnt += len(stack)-1
+            stack.pop()
+        elif s[i-1] == ')':
+            cnt += 1
+            stack.pop()
+print(cnt)
