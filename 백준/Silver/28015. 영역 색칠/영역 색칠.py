8508291aa175819b03f42c1553c8838807f94fa0
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-21 22:40:06 +0900

    [Silver II] Title: 영역 색칠, Time: 56 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/28015. 영역 색칠/영역 색칠.py
+++ 백준/Silver/28015. 영역 색칠/영역 색칠.py
@@ -1,56 +1,38 @@
+# min을 사용하는 이유는 예로 v==1일때 연속된 1의 시작을 의미하는데
+# 그 전에 row2가 0보다 크다면 이전에 연속된 2가 있었던 경우를 의미한다
+# 즉 cnt2를 증가시키는 것은이전에 연속된 2의 구간이 있었음을 기록
+# --> 2가 연속으로 있었고, 1이 등장함으로써 그 구간이 끝났음을 표시
+
 from collections import deque
 import sys
 input = sys.stdin.readline
 
 N, M = list(map(int, input().strip().split()))
-arr = [list(map(int, input().strip().split())) for _ in range(N)]
+paint = [list(map(int, input().strip().split())) for _ in range(N)]
 # print(arr)
 
 
 
 
-def get_min(stack):
-    row1 = 0
-    cnt1 = 0
-    row2 = 0
-    cnt2 = 0
-    
-    if not stack:
-        return 0
-    
-    while stack:
-        v = stack.pop()
-        if v == 1:
-            if row1 > 0:
-                cnt1 += 1
-            row1 = 0
-            row2 += 1
-        else:
-            row1 += 1
-            if row2 > 0:
-                cnt2 += 1
-            row2 = 0
-    
-    if row1 != 0:
-        cnt1 += row1
-    if row2 != 0:
-        cnt2 += row2
-    
-    return min(cnt1, cnt2) + 1
-
 res = 0
-for i in range(N):
-    stack = []  
-    cnt = 0
-    for j in range(M):
-        if arr[i][j] == 0:
-            cnt += get_min(stack)
-        else:
-            stack.append(arr[i][j])
-    
-    if stack:
-        cnt += get_min(stack)
-    
-    res += cnt
+
+for x in range(N):  
+    for y in range(M):  
+        if paint[x][y] != 0:
+            color = paint[x][y]
+            # 오른쪽으로 진행하며
+            for i in range(M - y):  # 색칠할 구역 체크
+                # 범위 체크를 추가
+                if paint[x][y + i] != 0:
+                    # 같은 색상이면 덧칠할 필요 없으므로 방문 체크
+                    if paint[x][y + i] == color:
+                        paint[x][y + i] = 0
+                # 0을 만나면 색칠 끝
+                else:
+                    res += 1
+                    break
+            # 오른쪽 끝에 도달하면 색칠 끝
+            else: 
+                res += 1
 
 print(res)
