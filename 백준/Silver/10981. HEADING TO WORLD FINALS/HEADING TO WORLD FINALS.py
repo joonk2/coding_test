47364f250f4f57901245fd69ef90fed5d81f3aa8
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-10-28 21:47:21 +0900

    [Silver IV] Title: HEADING TO WORLD FINALS, Time: 336 ms, Memory: 66236 KB -BaekjoonHub

--- 백준/Silver/10981. HEADING TO WORLD FINALS/HEADING TO WORLD FINALS.py
+++ 백준/Silver/10981. HEADING TO WORLD FINALS/HEADING TO WORLD FINALS.py
@@ -1,17 +1,18 @@
 import sys
 input = sys.stdin.readline
-N,K = map(int, input().strip().split())
+N,K = list(map(int, input().strip().split()))
 data = []
-for _ in range(N):
-    univ, team, solved, penalty = list(map(str, input().split()))
-    data.append((univ, team, int(solved), int(penalty)))
 
+for _ in range(N):
+    school, id, solved, penalty = list(map(str, input().strip().split()))
+    data.append((school, id, int(solved), int(penalty)))
 
 data.sort(key = lambda x: (-x[2], x[3]))
+
 ans = []
 for i in range(N):
     if len(ans) == K:
         break
     if data[i][0] not in ans:
-        print(data[i][1])
         ans.append(data[i][0])
+        print(data[i][1])
