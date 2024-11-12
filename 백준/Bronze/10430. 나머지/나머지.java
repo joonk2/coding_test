1a6cdca93c9258f67cba94fa61b376291f62c1ed
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-13 00:41:58 +0900

    Update 나머지.java

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,24 +1,22 @@
 import java.util.StringTokenizer;
-import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        // A, B, C 값 입력
+        // A, B, C 입력 값
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
 
-        // 연산 출력
-        System.out.println((A+B) % C);
-        System.out.println((A%C + B%C) % C);
-        System.out.println((A*B) % C);
-        System.out.println((A%C * B%C) % C);
-
-        
+        // 출력 값
+        System.out.println((A+B)%C);
+        System.out.println( ((A%C) + (B%C)) % C );
+        System.out.println( (A*B)%C );
+        System.out.println( ((A%C)*(B%C)) % C );
     }
 }
