db58b28f6904bb0999c65131a98634764f78cc21
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-12 21:03:37 +0900

    Update 평균 점수.java

--- 백준/Bronze/10039. 평균 점수/평균 점수.java
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -1,20 +1,18 @@
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.io.BufferedReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int sum = 0;
-
-        for (int i = 0; i < 5; i++) {
+        int sum = 0, K = 5;
+        for (int i = 0; i < K; i++) {
             int score = Integer.parseInt(br.readLine());
             if (score < 40) {
                 score = 40;
             }
             sum += score;
         }
-        System.out.println(sum/5);
+        System.out.println(sum/K);
     }
 }
