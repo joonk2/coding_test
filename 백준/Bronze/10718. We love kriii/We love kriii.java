1d28e013c10593db72cca25d3feac114c2a8b737
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-13 00:46:37 +0900

    [Bronze V] Title: We love kriii, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,14 @@
+//import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 결과 출력
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
