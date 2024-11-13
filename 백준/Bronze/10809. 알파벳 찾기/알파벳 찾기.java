0702e4b977f7483176721664021a08d7ee0457d0
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-13 16:30:45 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,39 +1,29 @@
-// 입출력 예제 설명
-
-// 아래는 String S
-// 0 1 2 3 4 5 6 7
-// b a c k j o o n
-
-// 아래는 arr
-// a b c d e f g h i j ... z
-// 0 1 2 3 4 5 6 7 8 9     25
-// 1 0-1-1 2-1-1-1-1 4     -1
-
 import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine();
 
-        // -1로 된 알파벳 갯수 배열 26개 생성
+        // -1로 된 알파벳 배열 갯수 26개 생성
         int arr[] = new int[26];
         for (int i = 0; i < 26; i++) {
             arr[i] = -1;
         }
 
-        // arr에 인덱스 맞추기
+        //S 문자열 인덱스 설정
         for (int i = 0; i < S.length(); i++) {
             char ch = S.charAt(i);
 
+            // arr에 매칭
             if (arr[ch - 'a'] == -1) {
-                arr[ch -'a'] = i;
+                arr[ch-'a'] = i;
             }
-
         }
+        // 출력
         for (int res : arr) {
             System.out.print(res + " ");
         }
