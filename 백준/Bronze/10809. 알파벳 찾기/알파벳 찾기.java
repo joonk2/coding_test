d6a00c27e18a9fbaa97c746f94f387ff28993927
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-13 15:31:28 +0900

    Update 알파벳 찾기.java

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -10,32 +10,33 @@
 // 1 0-1-1 2-1-1-1-1 4     -1
 
 import java.util.StringTokenizer;
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine();
 
-        // -1로 된 알파벳 갯수 26개 배열 생성
+        // -1로 된 알파벳 갯수 배열 26개 생성
         int arr[] = new int[26];
         for (int i = 0; i < 26; i++) {
             arr[i] = -1;
         }
 
+        // arr에 인덱스 맞추기
         for (int i = 0; i < S.length(); i++) {
             char ch = S.charAt(i);
 
             if (arr[ch - 'a'] == -1) {
-                arr[ch - 'a'] = i;
+                arr[ch -'a'] = i;
             }
+
+        }
+        for (int res : arr) {
+            System.out.print(res + " ");
         }
 
-        // 결과 출력
-        for (int var : arr) {
-            System.out.print(var + " ");
-        }
     }
 }
