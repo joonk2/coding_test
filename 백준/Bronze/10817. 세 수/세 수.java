e9be1c75e96dee5a97c21418579b8d5220ff771c
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-14 14:11:30 +0900

    Update 세 수.java

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -7,14 +7,13 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int[] arr = new int[3];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr);
         System.out.println(arr[1]);
     }
