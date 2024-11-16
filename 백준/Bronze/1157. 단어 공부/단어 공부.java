e8bddca32d10e108f5c14dfed9e874126d564836
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-16 16:47:49 +0900

    [Bronze I] Title: 단어 공부, Time: 244 ms, Memory: 21132 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,6 +1,6 @@
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,34 +9,36 @@ public class Main {
         String str = br.readLine().toUpperCase();
         int[] arr = new int[26];
 
+
         // str의 각 인덱스의 알파벳 등장횟수를 arr에 저장
         for (int i = 0; i < str.length(); i++) {
             arr[str.charAt(i) - 'A']++;
         }
 
+
         int max_value = 0;
-        char answer = '?';
+        char answer = '\n'; // 빈문자
 
         // 0 ~ 25까지 순회해서 zZa 일 경우 (char) (25 + 'A'); 라서 자동으로 Z
-        for ( int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (max_value < arr[i]) {
                 max_value = arr[i];
-                answer = (char) (i+ 'A');
+                answer = (char) (i + 'A');
             }
         }
-        
+
         // 제일 많이 나온 횟수 cnt++;
         // Mississipi일때 I=4, S=4라서 cnt=2 됨
         int cnt = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == max_value) {
+            if (max_value == arr[i]) {
                 cnt++;
             }
         }
 
         // cnt > 1 이면 '?', 그 외는 제일 많이 나온 알파벳 출력
         if (cnt > 1) {
-            System.out.println("?");
+            System.out.println('?');
         }
         else {
             System.out.println(answer);
