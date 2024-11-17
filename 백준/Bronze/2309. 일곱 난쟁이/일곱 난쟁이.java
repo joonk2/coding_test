b54b94e1d041d7ee2401f67b9a97f22aa78ffda5
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-17 23:26:29 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int[] arr = new int[9];
+        int sum = 0;
+
+        // 1. 입력 및 합계 계산
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+
+        // 2. 난쟁이 2마리의 키 0cm으로 변경
+        for (int i = 0; i < 8; i++) {
+            for (int j = i+1; j < 9; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    Arrays.sort(arr);
+
+                    for (int k = 2; k < 9; k++) {
+                        System.out.println(arr[k]);
+                    }
+
+                    // 원하는 것을 찾았으면, 3중 반복문 탈출
+                    return;
+
+                }
+            }
+        }
+
+
+
+    }
+}
