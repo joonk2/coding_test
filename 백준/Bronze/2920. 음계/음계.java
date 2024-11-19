859966555c38a4ba186da00c847b0786dc12bc3e
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-19 16:37:38 +0900

    [Bronze II] Title: 음계, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String S = br.readLine();
+        
+        if (S.equals("1 2 3 4 5 6 7 8")) {
+            System.out.println("ascending");
+        }
+        else if (S.equals("8 7 6 5 4 3 2 1")) {
+            System.out.println("descending");
+        }
+        else {
+            System.out.println("mixed");
+        }
+        
+        
+    }
+}
