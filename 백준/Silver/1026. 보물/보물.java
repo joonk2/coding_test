dfe3e0be1f65dbfc0c33c583901b1f782caa0253
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-20 00:41:18 +0900

    Update 보물.java

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -2,10 +2,10 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import java.util.StringTokenizer;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,25 +16,25 @@ public class Main {
         List<Integer> A = new ArrayList<>();
         List<Integer> B = new ArrayList<>();
 
-
         // arr A
-        st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             A.add(Integer.parseInt(st.nextToken()));
         }
 
-
         // arr B
-        st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             B.add(Integer.parseInt(st.nextToken()));
         }
 
-
         // res
         int res = converter(N,A,B);
         System.out.println(res);
+
     }
+
+
 
 
 
@@ -43,7 +43,6 @@ public class Main {
 
     public static int converter(int N, List<Integer> A, List<Integer> B) {
         Collections.sort(A);
-
         int sum = 0;
 
         for (int i = 0; i < N; i++) {
@@ -56,6 +55,7 @@ public class Main {
 
         return sum;
     }
+
 
 
 
