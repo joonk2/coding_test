bf6000c8a96a231b9aea7751877681615c602ee9
joonk2 <153247950+joonk2@users.noreply.github.com>
2024-11-20 15:40:02 +0900

    [Silver IV] Title: 한수, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -1,51 +1,43 @@
-//한수
-//숫자 3자리 abc라고 할 때 a-b = b-c 여야함
-//100미만 한수 -> 1~99 
-
-
-//210개의 한수
-//1~99 + 111, 123, 135, 147, 159, 210 --> 총 105개
-//매우 중요 ! --> 정수는 인덱스로 접근할 수 없다  (문자열 변환이 필요하겠네?)
-
-
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int cnt = 0;
-        
+
         for (int i = 1; i <= N; i++) {
             if (i < 100) {
                 cnt++;
             }
             else if (i >= 100 && i < 1000) {
                 int[] digits = digit_changer(i);
-                if (digits[0] - digits[1] == digits[1]-digits[2]) {
+                if (digits[0] - digits[1] == digits[1] - digits[2]) {
                     cnt++;
                 }
             }
         }
         System.out.println(cnt);
+
+
         
         
     }
-    
-    
     // int --> String
     // charAt() 자체가 Ascii 코드로 자동변환되는게 원칙이라 ex) 1 5 9 라면, '1'-'0' = 49-48
     public static int[] digit_changer(int num) {
         String str = String.valueOf(num);
         int[] digits = new int[str.length()];
+
         for (int i = 0; i < str.length(); i++) {
             digits[i] = str.charAt(i) - '0';
         }
+
         return digits;
-        
     }
-    
-    
+
+
+
 }
