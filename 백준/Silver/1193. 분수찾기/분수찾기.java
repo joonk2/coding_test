c65d73fb4c229ad9cd6c616ea580c853731b71f1
joonk2 <with.joonk@gmail.com>
2024-12-12 12:34:41 +0900

    Update 분수찾기.java

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,3 +1,36 @@
+/*
+X=4
+4-1 -> line=2
+3-2 -> line=3
+X=1, line=3
+1/3
+
+X=5
+5-1 -> line=2
+4-2 -> line=3
+X=2, line=3
+2/2
+
+X=6
+6-1 -> line=2
+5-2 -> line=3
+X=3, line=3
+
+X=7
+7-1 -> line=2
+6-2 -> line=3
+4-3 -> line=4
+X=1, line=4
+1/4
+
+X=8
+8-1 -> line=2
+7-2 -> line=3
+5-3 -> line=4
+X=2, line=4
+2/3
+ */
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
