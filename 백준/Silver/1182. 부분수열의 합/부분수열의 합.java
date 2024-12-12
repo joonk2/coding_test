1e70ea0091a4bbdd025d712fab45d40b825f98ef
joonk2 <with.joonk@gmail.com>
2024-12-12 09:59:35 +0900

    Update 부분수열의 합.java

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,5 +1,5 @@
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 import java.util.StringTokenizer;
@@ -9,31 +9,34 @@ public class Main {
     static int[] arr;
 
 
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
 
-        arr = new int[N+1];
-        st = new StringTokenizer(br.readLine());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        arr = new int[N];
+
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 계산
-        for (int r = 1; r < N+1; r++) {
-            combination(0,0,r,0);
+        // 조합 계산
+        for (int SeqLeng = 1; SeqLeng < N+1; SeqLeng++) {
+            combination(0, 0, SeqLeng, 0);
         }
 
         System.out.println(cnt);
 
 
+
     }
 
-
-    static void combination(int start, int depth, int r, int sum) {
-        if (depth == r) {
+    static void combination(int start, int depth, int SeqLeng, int sum) {
+        if (depth == SeqLeng) {
             if (sum == S) {
                 cnt++;
             }
@@ -41,12 +44,10 @@ public class Main {
         }
 
         for (int i = start; i < N; i++) {
-            combination(i+1, depth+1, r, sum+arr[i]);
+            combination(i+1, depth+1, SeqLeng, sum+arr[i]);
         }
 
     }
-
-
 
 
 }
