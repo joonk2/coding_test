4a36ef2fcda9150a57d066dc989276154da60496
joonk2 <with.joonk@gmail.com>
2025-01-07 23:19:32 +0900

    [Gold II] Title: 뉴스 전하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+#define sz(x) (int)x.size()
+#define pb push_back
+
+using namespace std;
+
+vector<int> adj[51];
+int dfs(int u) {
+	if (sz(adj[u]) == 0) {
+		return 0;
+	}
+	vector<int> vc;
+	for (int v : adj[u]) {
+		vc.pb(dfs(v));
+	}
+	sort(all(vc), greater<int>());
+	int mx = 0;
+	for (int i = 0; i < sz(vc); i++) {
+		mx = max(mx, vc[i] + i + 1);
+	}
+	return mx;
+}
+int main(void) {
+	ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a; cin >> a;
+		if (a != -1) adj[a].pb(i);
+	}
+	cout << dfs(0);
+}
