a1508dfe1ed9ee1299d944fab02637be8762c8a6
joonk2 <with.joonk@gmail.com>
2025-01-19 11:54:26 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -0,0 +1,57 @@
+/*
+0일 경우에 이미 지운 수니까 continue로 건너뛰면서,
+배수를 오름차순으로 증가시키면서 지워주자( != 0 이면 =0 처리)
+배수를 지우자 그리고 k--
+이제 k == 0 이면 그 수를 출력하고 return(종료)
+ */
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
+
+public class Main {
+	static StringTokenizer st;
+	static int N, K;
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine().trim());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		IsPrimeNumber(N, K);
+		
+	}
+	
+	public static void IsPrimeNumber(int N, int K) {
+		int[] arr = new int[N+1];
+		for (int i = 2; i < N+1; i++) {
+			arr[i] = i;
+		}
+		
+		for (int i = 2; i < N+1; i++) {
+			if (arr[i] == 0) {
+				continue;
+			}
+			for (int j = i; j < N+1; j += i) {
+				if (arr[j] != 0) {
+					arr[j] = 0;
+					K--;
+				}
+				
+				
+				if (K == 0) {
+					System.out.println(j);
+					return;
+				}
+			}
+		}
+		
+		
+	}
+	
+	
+	
+}
