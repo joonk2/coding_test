d6cd1d8f84ea3f2263509526613ce2d7bf53ca35
joonk2 <with.joonk@gmail.com>
2025-01-27 09:40:32 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.Arrays;
+
+public class Main {
+	static int N;
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String s = br.readLine();
+		sb.append(s+"??!");
+		System.out.print(sb);
+		
+		
+		
+	}
+}
