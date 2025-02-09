cfd938b3e3af82cc77452ec6d4c4a1c3a5772aa3
joonk2 <with.joonk@gmail.com>
2025-02-09 21:38:58 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1612 ms, Memory: 480636 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,33 +1,44 @@
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-
-import java.util.Arrays;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        Arrays.sort(arr);
-        Arrays.toString(arr);
-
-        for (int i = 0; i < N; i++) {
-            bw.write(arr[i] + "\n");
-        }
-        bw.flush();
-        bw.close();
-        br.close();
-
-
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// print optimization
+		StringBuilder sb = new StringBuilder(); 
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] cnt_lst = new int[10001]; // 1~10000
+		
+		// counting num
+		for (int i = 0; i < N; i++) {
+			cnt_lst[Integer.parseInt(br.readLine())]++;
+		}
+		
+		for (int i = 1; i < 10001; i++) {
+			while (cnt_lst[i]-- > 0) {
+				sb.append(i).append('\n'); 
+			}
+		} 
+		
+		System.out.print(sb);
+	}
 }
+
+
+/*
+10
+5
+2
+3
+1
+4
+2
+3
+5
+1
+7
+*/
