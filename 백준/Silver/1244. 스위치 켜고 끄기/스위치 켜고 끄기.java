279d0cec70d4474550168b9c07762f19562c8be6
joonk2 <with.joonk@gmail.com>
2025-02-12 19:53:30 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 68 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -2,67 +2,65 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 스위치 개수
-        int[] arr = new int[N + 1];
-
-        // 스위치 상태 입력
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        // 학생 수 입력
-        int M = Integer.parseInt(br.readLine());
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int gender = Integer.parseInt(st.nextToken());
-            int card = Integer.parseInt(st.nextToken());
-
-            // 남학생 처리
-            if (gender == 1) {
-                for (int j = card; j <= N; j += card) {
-                    arr[j] = 1 - arr[j];
-                }
-            }
-            // 여학생 처리
-            else if (gender == 2) {
-                int left = card;
-                int right = card;
-                while (left > 0 && right <= N && arr[left] == arr[right]) {
-                    if (left == right) {
-                        arr[left] = 1 - arr[left];
-                    } else {
-                        arr[left] = 1 - arr[left];
-                        arr[right] = 1 - arr[right];
-                    }
-                    left--;
-                    right++;
-
-                    // 범위 체크 수정: right가 N을 초과하면 종료
-                    if (left < 1 || right > N) {
-                        break;
-                    }
-                }
-            }
-        }
-
-       
-        for (int i = 1; i <= N; i++) {
-            System.out.print(arr[i]);
-            if (i % 20 == 0 || i == N) { // 20개씩 출력 또는 마지막 줄 처리
-                System.out.println();
-            } else {
-                System.out.print(" ");
-            }
-        }
-        
-        
-        
-        
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] arr = br.readLine().split(" ");
+		int student = Integer.parseInt(br.readLine());
+		
+		int[] lst = new int[N];
+		for (int i = 0; i < N; i++) {
+			lst[i] = Integer.parseInt(arr[i]);
+		}
+		
+		for (int i = 0; i < student; i++) {
+			String[] s_arr = br.readLine().split(" ");
+			int gender = Integer.parseInt(s_arr[0]);
+			int button_idx = Integer.parseInt(s_arr[1]);
+			
+			int idx = button_idx-1;
+			
+			if (gender == 1) {
+				for ( int k = idx; k < lst.length; k += (idx+1) ) {
+					lst[k] = 1-lst[k];
+				}
+			}
+			else if (gender == 2) {
+				// 어차피 양옆이 같으나 다르나 본인은 바꿔야한다
+				lst[idx] = 1-lst[idx];
+				
+				int x = 1;
+				while (idx-x >= 0 && idx+x < N) {
+					if (lst[idx - x] == lst[idx + x]) {
+						lst[idx - x] = 1- lst[idx - x];
+						lst[idx + x] = 1 - lst[idx + x];
+						x++;
+					}
+					else {
+						// 대칭이 달라지자마자 while문 종료
+						break;
+					}
+				}
+				
+			}
+					
+		}
+		
+		
+		
+		for (int k = 0; k < N; k++) {
+			System.out.print(lst[k] + " ");
+			if ((k+1)%20 == 0) {
+				System.out.println();
+				
+			}
+		}
+		
+		
+		
+		
+		
+	}
 }
