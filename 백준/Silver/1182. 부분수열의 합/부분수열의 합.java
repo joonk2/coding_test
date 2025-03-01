5d2509b93060e119009782228a275e291b1e8e92
joonk2 <with.joonk@gmail.com>
2025-03-01 15:56:29 +0900

    [Silver II] Title: 부분수열의 합, Time: 76 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,55 +1,69 @@
-// 시작점, 깊이, 수열길이, 중간합
+/*
+[시도 방법]
+1. 연속적인 부분수열합
+2. 비연속적인 부분수열 합 --> 백트랙킹
+
+[틀린 이유]
+1. 연속적인 부분수열만 고려했음
+반례 -->
+5 0
+-7 -3 -2 8 5
+
+*/
 
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
-
-import java.util.StringTokenizer;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int N, S, cnt = 0;
-    static int[] arr;
-
-
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-
-
-        st = new StringTokenizer(br.readLine(), " ");
-        arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        // 조합 계산
-        for (int SeqLeng = 1; SeqLeng < N+1; SeqLeng++) {
-            combination(0, 0, SeqLeng, 0);
-        }
-
-        System.out.println(cnt);
-
-
-
-    }
-
-    static void combination(int start, int depth, int SeqLeng, int sum) {
-        if (depth == SeqLeng) {
-            if (sum == S) {
-                cnt++;
-            }
-            return;
-        }
-
-        for (int i = start; i < N; i++) {
-            combination(i+1, depth+1, SeqLeng, sum+arr[i]);
-        }
-
-    }
-
-
+	static int N, S, sum, cnt;
+	static int[] lst;
+	
+	public static void main(String[] arsg) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = br.readLine().split(" ");
+		N = Integer.parseInt(arr[0]);
+		S = Integer.parseInt(arr[1]);
+		
+		String[] sequence = br.readLine().split(" "); 
+		
+		lst = new int[N];
+		for (int i = 0; i < N; i++) {
+			lst[i] = Integer.parseInt(sequence[i]);
+		}
+		
+        
+        // backtrack
+		cnt = 0;
+		backtrack(0,0, false);
+		System.out.println(cnt);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	public static void backtrack(int i, int sum, boolean isSelected) {
+        // 원소 5개까지 포함하여 다 돌아봤을 때
+		if (i == N) {
+			if (sum == S && isSelected == true) {
+				cnt++;
+			}
+			return;
+		}
+		
+		
+		// 1. 현재 원소를 선택할 때
+		backtrack(i+1, sum + lst[i], true);
+		
+		// 2. 현재 원소를 선택 안할 때
+		backtrack(i+1, sum, isSelected);
+		
+	}
+	
+	
+	
 }
