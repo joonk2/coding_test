3f8e6ce02af1aa8854700d63e645c161cd9b712f
joonk2 <with.joonk@gmail.com>
2025-03-26 20:53:14 +0900

    [Bronze II] Title: 방 배정, Time: 60 ms, Memory: 11416 KB, Score: 2 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -1,46 +1,67 @@
 /*
-[조건]
-1. 한 방에 인원제한은 K명
-2. 같은 학년 & 같은 성별일 것
- */
+[idea]
+make 2 different room
+[][] --> [gender : girl(0) or boy(1)][grade : 1 ~ 6]
+we dont count if there's no one in the room
+so let's set a range (a person(min) to K(max))
+
+if the arrays value exceed K, then devide by mod and remainder per boy and girl
+--> cnt += (mod + remainder)
+else: just cnt++
+*/
 
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-import java.util.StringTokenizer;
+//import java.util.Arrays;
 
 public class Main {
-    static int N, K, S, Y, cnt;
-    static int[][] room;
-    static StringTokenizer st;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine().trim());
-
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        room = new int[2][7];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine().trim());
-            S = Integer.parseInt(st.nextToken());
-            Y = Integer.parseInt(st.nextToken());
-            room[S][Y]++;
-        }
-
-        cnt = 0;
-        for (int i = 0; i < 2; i++) {
-            for (int j = 1; j < 7; j++) {
-                cnt += room[i][j] / K;
-                if (room[i][j] % K != 0) {
-                    cnt++;
-                }
-            }
-        }
-        System.out.println(cnt);
-
-
-    }
+	static int[][] room = new int[2][7];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] NK = br.readLine().split(" ");
+		int N = Integer.parseInt(NK[0]);
+		int K = Integer.parseInt(NK[1]);
+		
+		for (int i = 0; i < N; i++) {
+			String[] gender_grade = br.readLine().split(" ");
+			int gender = Integer.parseInt(gender_grade[0]);
+			int grade = Integer.parseInt(gender_grade[1]);
+			
+			room[gender][grade]++;
+		}
+		
+		int girl = 0;
+		int boy = 1;
+		int cnt = 0;
+		for (int i = 1; i < 6+1; i++) {
+			if (room[girl][i] >= 1 && room[girl][i] <= K) {
+				cnt++;
+			}
+			else if (room[girl][i] > K) {
+				int g_mod = (room[girl][i] / K);
+				int g_remainder = (room[girl][i] % K);
+				cnt += (g_mod + g_remainder);
+			}
+			
+			if (room[boy][i] >= 1 && room[boy][i] <= K) {
+				cnt++;
+			}
+			else if (room[boy][i] > K) {
+				int b_mod = (room[boy][i] / K);
+				int b_remainder = (room[boy][i] % K);
+				cnt += (b_mod + b_remainder);
+			}
+			
+		}
+		
+		System.out.println(cnt);
+//		System.out.println(Arrays.deepToString(room));
+		
+		
+		
+	}
+	
 }
