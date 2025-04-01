04388eb741470fd57c7408aefee02e9ab2962e40
joonk2 <with.joonk@gmail.com>
2025-04-01 22:03:10 +0900

    [Silver IV] Title: 빙고, Time: 72 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -1,137 +1,141 @@
-/*
-1. 빙고 경우의 수를 4개 생각하라
-(가로, 세로는 각각 5개, 대각선은 가능할 경우가 각각 1개)
-2. 현재 선택중인 숫자 기록 (0으로 변경하여 세보자)
-3. 3줄되면 즉시 종료하면서 당시 선택된 숫자 출력
- */
-
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-import java.util.StringTokenizer;
+//import java.util.Arrays;
 
 public class Main {
-    static int cnt, cnt_zero;
-    static int[][] bingo = new int[5][5];
-    static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[][] bingo = new int[5][5];
+		for (int r = 0; r < 5; r++) {
+			String[] col = br.readLine().split(" ");
+			for (int c = 0; c < 5; c++) {
+				bingo[r][c] = Integer.parseInt(col[c]);
+			}
+		}
+		
+		
+		// check
+		int cnt = 0;
+		int[][] num_arr = new int[5][5];
+		for (int r = 0; r < 5; r++) {
+			String[] col = br.readLine().split(" ");
+			for (int c = 0; c < 5; c++) {
+				num_arr[r][c] = Integer.parseInt(col[c]);
+				int cur_num = num_arr[r][c];
+				
+				
+				for (int y = 0; y < 5; y++) {
+					for (int x = 0; x < 5; x++) {
+						if (cur_num == bingo[y][x]) {
+							bingo[y][x] = 0;
+							cnt++;
+						}
+					}
+				}
+				
+				
+				int cnt_row = 0;
+				int cnt_col = 0;
+				int cnt_diag_left = 0;
+				int cnt_diag_right = 0;
+				
+				// row check
+				for (int y = 0; y < 5; y++) {
+					int temp_row_cnt = 0;
+					for (int x = 0; x < 5; x++) {
+						if (bingo[y][x] == 0) {
+							temp_row_cnt++;
+						}
+					}
+					if (temp_row_cnt == 5) {
+						cnt_row++;
+					}
+				}
+				
+				
+				
+				// break
+				if (cnt_row + cnt_col + cnt_diag_left + cnt_diag_right >= 3) {
+					System.out.println(cnt);
+					return;
+				}
+				
+				
+				// col check
+				for (int y = 0; y < 5; y++) {
+					int temp_col_cnt = 0;
+					for (int x = 0; x < 5; x++) {
+						if (bingo[x][y] == 0) {
+							temp_col_cnt++;
+						}
+					}
+					if (temp_col_cnt == 5) {
+						cnt_col++;
+					}
+				}
+				
+				
+				
+				// break
+				if (cnt_row + cnt_col + cnt_diag_left + cnt_diag_right >= 3) {
+					System.out.println(cnt);
+					return;
+				}
+				
+				
+				// diag_left
+				int temp_diag_left_cnt = 0;
+				for (int y = 0; y < 5; y++) {
+					if (bingo[y][y] == 0) {
+						temp_diag_left_cnt++;
+					}
+					if (temp_diag_left_cnt == 5) {
+						cnt_diag_left++;
+					}
+				}
+				
+				
+				// break
+				if (cnt_row + cnt_col + cnt_diag_left + cnt_diag_right >= 3) {
+					System.out.println(cnt);
+					return;
+				}
+				
+				
+				// diag_right
+				int temp_diag_right_cnt = 0;
+				for (int y = 0; y < 5; y++) {
+					if (bingo[y][4-y] == 0) {
+						temp_diag_right_cnt++;
+					}
+					if (temp_diag_right_cnt == 5) {
+						cnt_diag_right++;
+					}
+				}
+				
+				
+				// break
+				
+				if (cnt_row + cnt_col + cnt_diag_left + cnt_diag_right >= 3) {
+					System.out.println(cnt);
+					return;
+				}
+				
+				
+				
+			}
+		}
+				
+		
+		
 
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        cnt = 0;
-
-        // input
-        for (int i = 0; i < 5; i++) {
-            st = new StringTokenizer(br.readLine().trim());
-            for (int j = 0; j < 5; j++) {
-                bingo[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        // make a cur_num and adapt'em to mark_num()
-        // k = on what num, 3 sets of bingo completed?
-        int k = 0;
-        for (int i = 0; i < 5; i++) {
-            st = new StringTokenizer(br.readLine().trim());
-            for (int j = 0; j < 5; j++) {
-                int cur_num = Integer.parseInt(st.nextToken());
-                k++;
-
-                // changing a cur_num to 0
-                mark_num(cur_num);
-
-                // bingo check
-                r_check();
-                c_check();
-                lr_check();
-                rl_check();
-
-                // if 3 bingo is completed, then finish
-                if (cnt >= 3) {
-                    System.out.println(k);
-                    return;
-                }
-
-                // reset with cnt of bingo
-                cnt = 0;
-
-            }
-        }
-
-
-
-    }
-
-
-    // mark_num
-    // all cur_num -> 0
-    public static void mark_num(int cur_num) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (bingo[i][j] == cur_num) {
-                    bingo[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    // row check
-    public static void r_check() {
-        for (int i = 0; i < 5; i++) {
-            cnt_zero = 0;
-            for (int j = 0; j < 5; j++) {
-                if (bingo[j][i] == 0) {
-                    cnt_zero++;
-                }
-            }
-            if (cnt_zero == 5) {
-                cnt++;
-            }
-        }
-    }
-
-    // col check
-    public static void c_check() {
-        for (int i = 0; i < 5; i++) {
-            cnt_zero = 0;
-            for (int j = 0; j < 5; j++) {
-                if (bingo[i][j] == 0) {
-                    cnt_zero++;
-                }
-            }
-            if (cnt_zero == 5) {
-                cnt++;
-            }
-        }
-    }
-
-    // lr check
-    public static void lr_check() {
-        cnt_zero = 0;
-        for (int i = 0; i < 5; i++) {
-            if (bingo[i][i] == 0) {
-                cnt_zero++;
-            }
-            if (cnt_zero == 5) {
-                cnt++;
-            }
-        }
-    }
-
-    // rl check
-    public static void rl_check() {
-        cnt_zero = 0;
-        for (int i = 0; i < 5; i++) {
-            if (bingo[i][4-i] == 0) {
-                cnt_zero++;
-            }
-            if (cnt_zero == 5) {
-                cnt++;
-            }
-        }
-    }
-
-
-
+		
+		
+		
+//		System.out.println(Arrays.deepToString(bingo));
+		
+		
+	}
 }
