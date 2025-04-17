89a529915509f687b6d35efaadbe05ebc400d9e4
joonk2 <with.joonk@gmail.com>
2025-04-18 01:20:58 +0900

    [Bronze II] Title: 줄 세우기, Time: 72 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.java
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.java
@@ -1,33 +1,28 @@
-/*
-[ (0,1) ]
-[(0,2) (0,1) ]
- */
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-import java.util.StringTokenizer;
+// import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String[] card_num_arr = br.readLine().split(" ");
+		
+		
+		ArrayList<Integer> student_lst = new ArrayList<>();
+		int[] card_num = new int[N];
+		for (int i = 0; i < N; i++) {
+			card_num[i] = Integer.parseInt(card_num_arr[i]);
+			student_lst.add(i-card_num[i], i+1);
+		}
+		
+		for (int i : student_lst) {
+			System.out.print(i + " ");
+		}
 
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            arr.add(i-num, i+1);
-        }
-
-
-        for (int a : arr) {
-            System.out.print(a + " ");
-        }
-
-
-    }
+				
+	}
 }
