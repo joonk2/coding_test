7f47ad12fc4040a63d6b0603725eba22fc87c71b
joonk2 <with.joonk@gmail.com>
2025-04-17 22:58:10 +0900

    [Silver V] Title: 색종이, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,37 +1,42 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        boolean[][] arr = new boolean[101][101];
-
-        int N = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine().trim());
-            int X = Integer.parseInt(st.nextToken());
-            int Y = Integer.parseInt(st.nextToken());
-
-            for (int j = X; j < 10 + X; j++) {
-                for (int k = Y; k < 10 + Y; k++) {
-                    if (!arr[j][k]) {
-                        arr[j][k] = true;
-                        cnt++;
-                    }
-                }
-            }
-
-            
-            
-        }
-        System.out.println(cnt);
-
-        
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		boolean[][] paper = new boolean[100][100];
+		
+		for (int i = 0; i < N; i++) {
+			String[] xy = br.readLine().split(" ");
+			int x = Integer.parseInt(xy[0]);
+			int y = Integer.parseInt(xy[1]);
+			
+			for (int j = x; j < x+10; j++) {
+				for (int k = y; k < y+10; k++) {
+					paper[j][k] = true;
+				}
+			}
+			
+		}
+		
+		
+		
+		int cnt = 0;
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				if (paper[i][j] == true) {
+					cnt++;
+				}
+			}
+		}
+		
+		
+		
+		System.out.println(cnt);
+		
+		
+	}
 }
