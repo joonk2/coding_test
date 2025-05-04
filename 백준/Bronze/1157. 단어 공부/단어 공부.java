650eac4facbab29a0381eeffe6c15136ea366cd5
joonk2 <with.joonk@gmail.com>
2025-05-04 13:51:48 +0900

    [Bronze I] Title: 단어 공부, Time: 176 ms, Memory: 23748 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,49 +1,43 @@
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
+//import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String word = br.readLine().toUpperCase();
+//		System.out.println(word);
+		
+		int[] cnt = new int[26];
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			cnt[ch-'A']++;
+		}
+//		System.out.println(Arrays.toString(cnt));
+		
+		
+		
+		int max_val = -99999;
+		char res = '?';
+		for (int i = 0 ; i < 26; i++) {
+			if (cnt[i] > max_val) {
+				max_val = cnt[i];
+				res = (char) (i + 'A');
+			}
+			else if (cnt[i] == max_val) {
+				res = '?';
+			}
+		}
+		
+		System.out.println(res);
+		
+		
+//		System.out.println((char)(3 + 'A'));
+//		System.out.println((char)(4 + 'A'));
+//		System.out.println((char)(5 + 'A'));
 
-        String str = br.readLine().toUpperCase();
-        int[] arr = new int[26];
-
-
-        // str의 각 인덱스의 알파벳 등장횟수를 arr에 추가하며 저장
-        for (int i = 0; i < str.length(); i++) {
-            arr[str.charAt(i) - 'A']++;
-        }
-
-
-        int max_value = 0;
-        char answer = '\n'; // 빈문자
-
-        // 0 ~ 25까지 순회해서 zZa 일 경우 (char) (25 + 'A'); 라서 자동으로 Z
-        for (int i = 0; i < arr.length; i++) {
-            if (max_value < arr[i]) {
-                max_value = arr[i];
-                answer = (char) (i + 'A');
-            }
-        }
-
-        // 제일 많이 나온 횟수 cnt++;
-        // Mississipi일때 I=4, S=4라서 cnt=2 됨
-        int cnt = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (max_value == arr[i]) {
-                cnt++;
-            }
-        }
-
-        // cnt > 1 이면 '?', 그 외는 최빈출 알파벳 출력
-        if (cnt > 1) {
-            System.out.println('?');
-        }
-        else {
-            System.out.println(answer);
-        }
-
-
-    }
+		
+	}
 }
