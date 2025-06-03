0f2f62e36dc1743e476e0f3e8330ef4de58544ab
joonk2 <with.joonk@gmail.com>
2025-06-03 23:19:39 +0900

    [Bronze V] Title: 개수 세기, Time: 68 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,25 +1,32 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import java.util.Arrays;
+
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		String[] s_arr = br.readLine().split(" ");
-		int v = Integer.parseInt(br.readLine());
+		String[] lst = br.readLine().split(" ");
+		int target_num = Integer.parseInt(br.readLine());
 		
-		int[] arr = new int[N];
+		int[] num_lst = new int[lst.length];
+//		System.out.println(num_lst.length);
+		
 		int cnt = 0;
-		
-		for (int i = 0; i < s_arr.length; i++) {
-			arr[i] = Integer.parseInt(s_arr[i]);
-			if (arr[i] == v) {
+		for (int i = 0; i < N; i++) {
+			num_lst[i] = Integer.parseInt(lst[i]);
+			if (num_lst[i] == target_num) {
 				cnt++;
 			}
 		}
-		
+//		System.out.println(Arrays.toString(num_lst));
 		System.out.println(cnt);
+		
+		
+		
 		
 	}
 }
