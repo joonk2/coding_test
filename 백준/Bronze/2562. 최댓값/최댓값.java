7913c02bc4f5c95a83f0f570da9302a5536b5d95
joonk2 <with.joonk@gmail.com>
2025-06-03 23:46:10 +0900

    [Bronze III] Title: 최댓값, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,28 +1,39 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-import java.util.StringTokenizer;
+//import java.util.Arrays;
+
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int max = Integer.MIN_VALUE;
-		int idx = 0;
-		
-		for (int i = 0; i < 9; i++) {
+		int N = 9;
+		int[] num_lst = new int[9];
+		int max_val = Integer.MIN_VALUE;
+		for (int i = 0; i < N; i++) {
 			int num = Integer.parseInt(br.readLine());
-			
-			if (num > max) {
-				max = num;
-				idx = i+1;
+			num_lst[i] = num;
+			if (num > max_val) {
+				max_val = num;
 			}
-		
-		
 		}
 		
-		System.out.println(max);
-		System.out.println(idx);
+		
+//		System.out.println(Arrays.toString(num_lst));
+		
+		for (int i = 0; i < num_lst.length; i++) {
+			if (max_val == num_lst[i]) {
+				System.out.println(max_val);
+				System.out.println(i+1);
+			}
+		}
+		
+
+		
+		
+		
+		
 	}
 }
