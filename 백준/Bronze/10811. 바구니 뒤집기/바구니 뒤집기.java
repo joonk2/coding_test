c166611000d0dd7284535011a1afd838702a3768
joonk2 <with.joonk@gmail.com>
2025-06-04 11:41:01 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 64 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,41 +1,51 @@
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
+
+//import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] s_arr = br.readLine().split(" ");
-		int N = Integer.parseInt(s_arr[0]);
-		int M = Integer.parseInt(s_arr[1]);
-		StringBuilder sb = new StringBuilder();
+		String[] NM = br.readLine().split(" ");
+		int N = Integer.parseInt(NM[0]);
+		int M = Integer.parseInt(NM[1]);
 		
+		int size = ++N;
+		int[] box = new int[size];
+//		System.out.println(Arrays.toString(box));
 		
-		int[] bucket = new int[N];
-		for (int i = 0; i < N; i++) {
-			bucket[i] = i+1;
+		for (int i = 0; i < size; i++) {
+			box[i] = i;
 		}
 		
 		for (int k = 0; k < M; k++) {
-			String[] arr = br.readLine().split(" ");
-			int i = Integer.parseInt(arr[0])-1;
-			int j = Integer.parseInt(arr[1])-1;
+			String[] ij = br.readLine().split(" ");
+			int i = Integer.parseInt(ij[0]);
+			int j = Integer.parseInt(ij[1]);
 			
 			while (i < j) {
-				int temp = bucket[i];
-				bucket[i] = bucket[j];
-				bucket[j] = temp;
+				int temp = box[i];
+				box[i] = box[j];
+				box[j] = temp;
 				i++;
 				j--;
 			}
+			
 		}
+//		System.out.println(Arrays.toString(box));
 		
-		for (int n : bucket) {
-			sb.append(n).append(" ");
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i < size; i++) {
+			if (i != size-1) {
+				sb.append(box[i] + " ");
+			}
+			else {
+				sb.append(box[i]);
+			}
+			
 		}
-		
 		System.out.println(sb);
-		
 		
 	}
 }
