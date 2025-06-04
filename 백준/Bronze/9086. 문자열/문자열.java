cb2a713980c486c70cc437bb90b4fd1e0f8031f6
joonk2 <with.joonk@gmail.com>
2025-06-04 12:11:46 +0900

    [Bronze V] Title: 문자열, Time: 60 ms, Memory: 11348 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,17 +1,26 @@
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
+
+//import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		while (T > 0) {		
-			String S = br.readLine();
-			System.out.println(S.charAt(0) + "" + S.charAt(S.length()-1));
-			T--;
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			String words = br.readLine();
+			if (words.length() > 1) {
+				char first_ch = words.charAt(0);
+				char last_ch = words.charAt(words.length()-1);
+				System.out.println(first_ch + "" +  last_ch);
+			}
+			if (words.length() == 1) {
+				char first_ch = words.charAt(0);
+				char last_ch = words.charAt(0);
+				System.out.println(first_ch + "" + last_ch);
+			}
+			
 		}
 		
 	}
