fba20f2e1b60f49e7a843a8be95a0430886df658
joonk2 <with.joonk@gmail.com>
2025-06-04 12:01:46 +0900

    [Bronze I] Title: 평균, Time: 76 ms, Memory: 11924 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,40 +1,34 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        String[] arr = br.readLine().split(" ");
-        int[] score = new int[N];
-        int max_score = 0;
-
-
-        // score 배열에 arr의 각 점수 할당
-        for (int i = 0; i < N; i++) {
-            score[i] = Integer.parseInt(arr[i]);
-            if (score[i] > max_score) {
-                max_score = score[i];
-            }
-        }
-
-
-        // 소수점을 포함하여 구하기 위해 double 사용
-        double sum = 0;
-
-
-        // 점수 수정 후 합산 (각 성적 / 최고 성적 * 100)
-        for (int i = 0; i < N; i++) {
-            sum += (double) score[i] / max_score * 100;
-        }
-
-
-        // 계산
-        System.out.println(sum / N);
-
-
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String[] scores_lst = br.readLine().split(" ");
+		double[] scores = new double[scores_lst.length];
+//		System.out.println(Arrays.toString(scores));
+		
+		double max_val = Integer.MIN_VALUE;
+		for (int i = 0; i < scores_lst.length; i++) {
+			scores[i] = Integer.parseInt(scores_lst[i]);
+			if (scores[i] > max_val) {
+				max_val = scores[i];
+			}
+		}
+//		System.out.println(Arrays.toString(scores));
+		
+		double total_score = 0;
+		for (int i = 0; i < scores.length; i++) {
+			scores[i] = (scores[i]/max_val)*100;
+//			System.out.println(scores[i]);
+			total_score += scores[i];
+		}
+		System.out.println(total_score/N);
+		
+		
+	}
 }
