6e7a0208d95dcb24b163830c8bdefb84b82bbd64
joonk2 <with.joonk@gmail.com>
2025-06-05 14:23:45 +0900

    [Bronze II] Title: 상수, Time: 68 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,30 +1,67 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+//import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] S = br.readLine().split(" ");
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] two_nums = br.readLine().split(" ");
+		
+		String first_num = two_nums[0];
+		String second_num = two_nums[1];
+		
+		// String -> char[]
+		// "734" -> ['7', '3', '4']
+		char[] first_num_arr = first_num.toCharArray();
+		char[] second_num_arr = second_num.toCharArray();
+		
+		// 디버깅
+//		System.out.println(Arrays.toString(first_num_arr));
+//		System.out.println(Arrays.toString(second_num_arr));
 
-        String A = S[0];
-        String B = S[1];
-
-        int reversed_A = reverse_num(A);
-        int reversed_B = reverse_num(B);
-
-        System.out.println(Math.max(reversed_A, reversed_B));
-    }
-
-
-    
-    public static int reverse_num(String N) {
-        StringBuilder sb = new StringBuilder(N);
-        return Integer.parseInt(sb.reverse().toString());
-    }
-
-    
-    
-
+		
+		// 거꾸로 뒤집는 함수 적용
+		my_reverse(first_num_arr);
+		my_reverse(second_num_arr);
+		
+		// 디버깅
+//		System.out.println(first_num_arr);
+//		System.out.println(second_num_arr);
+		
+		
+		// char -> String 그 이후 int 형으로 새로 할당
+		int reversed_first_num = Integer.parseInt(new String(first_num_arr));
+		int reversed_second_num = Integer.parseInt(new String(second_num_arr));
+		
+		// 결과
+		int res = 0;
+		if (reversed_first_num > reversed_second_num) {
+			res = reversed_first_num;
+		}
+		else {
+			res = reversed_second_num;
+		}
+		System.out.println(res);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	// 거꾸로 뒤집는 함수 
+	public static void my_reverse(char[] arr) {
+		for (int i = 0; i < arr.length / 2; i++) {
+			char temp = arr[i];
+			arr[i] = arr[arr.length-1-i];
+			arr[arr.length-1-i] = temp;
+		}
+	}
+	
 }
