4445b402d48ca244ae91f92844866c43fce77e30
joonhwan.K <with.joonk@gmail.com>
2025-06-14 13:30:23 +0900

    [Bronze II] Title: 벌집, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,32 +1,31 @@
+import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        System.out.println(Solution(N));
-    }
-
-
-    private static int Solution(int N) {
-        int room = 1;
-        int cnt = 1;
-
-        if (N == 1) {
-            return 1;
-        }
-
-        while (N > room) {
-            room += (cnt*6);
-            cnt++;
-        }
-
-        return cnt;
-    }
-
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if (N == 1) {
+			System.out.println(1);
+		}
+		else {
+			int i = 1;
+			int start = 1;
+			int end = 7;
+			while (true) {
+				if ( start < N && N <= end) {
+					i++;
+					break;
+				}
+				else {
+					start += (6*i);
+					i++;
+					end += (6*i);
+				}
+			}
+			System.out.println(i);
+			
+		}
+	}
 }
-
