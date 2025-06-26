f27a44c5e1bcfa84fea5da7e1187f687ab0f37f3
joonhwan.K <with.joonk@gmail.com>
2025-06-26 20:50:13 +0900

    Update 예산.java

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,76 +1,82 @@
-/*
-[접근방법]
-1. 그리디 
-2. 이분탐색
-
-*/
-
-
-
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 import java.util.Arrays;
 
-
 public class Main {
-	static int max_cost, N;
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		String[] cost_arr = br.readLine().split(" ");
-		int[] cost = new int[cost_arr.length];
+		String[] budget_arr = br.readLine().split(" ");
+		int[] budgets = str_to_int(budget_arr);
+		long total_budget = Long.parseLong(br.readLine());
 		
-		for (int i = 0; i < cost_arr.length; i++) {
-			cost[i] = Integer.parseInt(cost_arr[i]);
-		}
-		
-		// total_national_budget
-		int M = Integer.parseInt(br.readLine());
-		
-		// 이분 탐색을 위해 미리 asc 정렬하자
-		Arrays.sort(cost);
-		
-		
-		int left = 0;
-		int right = cost[N-1];
-		int res = 0;
-		
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			long sum = 0;
-			
-			// 금액 배분
-			for (int i = 0; i < N; i++ ) {
-				if (cost[i] > mid) {
-					sum += mid;
-				}
-				else {
-					sum += cost[i];
-				}
-			}
-			
-			
-			// 예산 키우기
-			if (sum <= M) {
-				res = mid;
-				left = mid + 1;
-			}
-			else {
-				right = mid - 1;
-			}
-			
-		}
-		
+		int res = parametric_search(budgets, total_budget);
 		System.out.println(res);
 		
 		
 		
 	}
 	
+	public static int parametric_search(int[] budgets, long total_budget) {
+		int left = 1;
+		int right = upperBound(budgets);
+		int the_maximum_out_of_budget = 0;
+		
+		while (left <= right) {
+			int mid = (left + right) / 2;
+//			System.out.println("mid -->" + mid);
+			
+			
+			if (can_increase_budget(mid, budgets, total_budget)) {
+				the_maximum_out_of_budget = mid;
+				left = mid + 1;
+			}
+			else if (!can_increase_budget(mid, budgets, total_budget)) {
+				right = mid - 1;
+			}
+		}
+		
+		return the_maximum_out_of_budget;
+	}
 	
+	
+	public static boolean can_increase_budget(int mid, int[] budgets, long total_budget) {
+		long temp_total_budgets = 0;
+		for (int i : budgets) {
+			if (i > mid) {
+				i = mid;
+			}
+			temp_total_budgets += i;
+		}
+//		System.out.println("temp_total_budgets -->" + temp_total_budgets);
+		return temp_total_budgets <= total_budget;
+	}
+	
+	
+	
+	
+	
+	public static int[] str_to_int(String[] arr) {
+		int[] budgets = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			budgets[i] = Integer.parseInt(arr[i]);
+		}
+		Arrays.sort(budgets);
+		return budgets;
+	} 
+	
+	
+	public static int upperBound(int[] budgets) {
+		int max = 0;
+		for (int i : budgets) {
+			if (i > max) {
+				max = i;
+			}
+		}
+		return max;
+	}
 	
 	
 	
