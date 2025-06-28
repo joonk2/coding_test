18c1b033a69822ede7750fd33951a9e939bd03e9
joonhwan.K <with.joonk@gmail.com>
2025-06-28 17:05:41 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 64 ms, Memory: 11392 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/README.md
+++ 백준/Bronze/5073. 삼각형과 세 변/README.md
@@ -4,35 +4,35 @@
 
 ### 성능 요약
 
-메모리: 14224 KB, 시간: 104 ms
+메모리: 11392 KB, 시간: 64 ms
 
 ### 분류
 
-기하학, 구현, 수학
+수학, 구현, 기하학
 
 ### 제출 일자
 
-2024년 11월 19일 17:24:12
+2025년 6월 28일 17:05:35
 
 ### 문제 설명
 
-<p>We know that triangles have 3 sides. Triangles may be classified according to the lengths of those sides, as shown in this table:</p>
+<p>삼각형의 세 변의 길이가 주어질 때 변의 길이에 따라 다음과 같이 정의한다.</p>
 
 <ul>
-	<li>Equilateral: All three sides are of equal length.</li>
-	<li>Isosceles: Two sides are of equal length, the other side is different.</li>
-	<li>Scalene: All three sides have different lengths.</li>
+	<li>Equilateral :  세 변의 길이가 모두 같은 경우</li>
+	<li>Isosceles : 두 변의 길이만 같은 경우</li>
+	<li>Scalene : 세 변의 길이가 모두 다른 경우</li>
 </ul>
 
-<p>There is one other possibility. It is possible to give three lengths of sides which would not form a valid traingle. For example 6, 3, 2. If the sum of the lengths of the two shortest sides is not greater than the length of the longest side, then the sides do not represent a triangle – the data is invalid.</p>
+<p>단 주어진 세 변의 길이가 삼각형의 조건을 만족하지 못하는 경우에는 "Invalid" 를 출력한다. 예를 들어 6, 3, 2가 이 경우에 해당한다. 가장 긴 변의 길이보다 나머지 두 변의 길이의 합이 길지 않으면 삼각형의 조건을 만족하지 못한다.</p>
 
-<p>In this problem you will be given values for the three lengths of the sides of a triangle and asked to classify it. </p>
+<p>세 변의 길이가 주어질 때 위 정의에 따른 결과를 출력하시오.</p>
 
 ### 입력 
 
- <p>Input consists of a number of lines, each line containing three positive integers, not more than 1,000, separated by single spaces. The last line of input will be 0 0 0 – do not process this line.</p>
+ <p>각 줄에는 1,000을 넘지 않는 양의 정수 3개가 입력된다. 마지막 줄은 0 0 0이며 이 줄은 계산하지 않는다.</p>
 
 ### 출력 
 
- <p>For each line of input, produce one line of output containing a single word (Equilateral, Isosceles, Scalene or Invalid) which is the classification of the lengths shown in the input.</p>
+ <p>각 입력에 맞는 결과 (Equilateral, Isosceles, Scalene, Invalid) 를 출력하시오.</p>
 
--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -4,49 +4,75 @@ import java.io.BufferedReader;
 
 import java.util.Arrays;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-
-        while (true) {
-            String[] S = br.readLine().split(" ");
-            int[] sides = new int[3];
-
-            for (int i = 0; i < 3; i++) {
-                sides[i] = Integer.parseInt(S[i]);
-            }
-
-            if (sides[0] == 0 && sides[1] == 0 && sides[2] == 0) {
-                break;
-            }
-
-
-
-            Arrays.sort(sides);
-            int a = sides[0], b = sides[1], c = sides[2];
-            
-
-            if (c >= a+b) {
-                sb.append("Invalid").append("\n");
-            }
-            else if (a == b && b == c) {
-                sb.append("Equilateral").append("\n");
-            }
-            else if (a == b || b == c || a == c) {
-                sb.append("Isosceles").append("\n");
-            }
-            else {
-                sb.append("Scalene").append("\n");
-            }
-
-
-        }
-
-        System.out.println(sb.toString());
-
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while (true) {
+			String[] xyz = br.readLine().split(" ");
+			int[] triangle_sides = str_to_int(xyz);
+			
+			if (isValid_angle(triangle_sides)) {
+				String res = my_detecter(triangle_sides);
+				System.out.println(res);
+			}
+			else if (!isValid_angle(triangle_sides)) {
+				break;
+			}
+		}
+		
+	}
+	
+	
+	
+	public static String my_detecter(int[] int_arr) {
+		String res = "";
+		
+		int x = int_arr[0];
+		int y = int_arr[1];
+		int z = int_arr[2];
+		
+		
+		if (x == y && y == z && z == x) {
+			res = "Equilateral";
+		}
+		else if (x + y <= z) {
+			res = "Invalid";
+		}
+		else if ( (x==y && y != z) ||  (y == z && z != x) || (z == x && x != y) ) {
+			res = "Isosceles";
+		}
+		else if (x != y && y != z && z != x) {
+			res = "Scalene";
+		}
+		
+		return res;
+	}
+	
+	
+	
+	
+	
+	public static boolean isValid_angle(int[] int_arr) {
+		int cnt = 0;
+		for (int i = 0; i < int_arr.length; i++) {
+			if (int_arr[i] == 0) cnt++;
+		}
+		if (cnt == 3) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
+	public static int[] str_to_int(String[] arr) {
+		int[] res = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			res[i] = Integer.parseInt(arr[i]);
+		}
+		Arrays.sort(res);
+		return res;
+	}
+	
 }
