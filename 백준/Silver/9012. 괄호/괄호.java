42d9994cedf7b95be47528a468aecd3c48ff63af
joonhwan.K <with.joonk@gmail.com>
2025-08-17 13:57:23 +0900

    [Silver IV] Title: 괄호, Time: 68 ms, Memory: 11636 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,38 +2,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-import java.util.Stack;
-
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc < T+1; tc++) {
-			String round_brackets = br.readLine();
-			String res = my_detector(round_brackets);
+			String round_bracket = br.readLine();
+			String res = my_detector(round_bracket);
 			System.out.println(res);
 		}
 	}
 	
 	
 	
-	
-	
-	public static String my_detector(String round_brackets) {
-		Stack<Character> my_container = new Stack<>();
-		for (int i = 0; i < round_brackets.length(); i++) {
-			char temp_char = round_brackets.charAt(i);
-			if (temp_char == '(') my_container.push(temp_char);
-			if (temp_char == ')') {
-				if (my_container.isEmpty()) return "NO";
-				if (i == round_brackets.length()-1 && my_container.size() > 1) return "NO";
-				if (my_container.peek() == '(') my_container.pop();
-			}
-			
+	public static String my_detector(String s) {
+		int cnt = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char temp_char = s.charAt(i);
+			if (temp_char == '(') cnt++;
+			if (temp_char == ')') cnt--;
+			if (cnt == -1) return "NO";
 		}
-		if (my_container.size() > 0) return "NO";
-		return "YES";
+		if (cnt == 0) return "YES";
+		else return "NO";
 	}
+	
+	
+	
 	
 	
 }
