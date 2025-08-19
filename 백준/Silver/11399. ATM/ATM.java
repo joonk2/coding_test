c3474f247241b126f3bcd2712174a539a98fb1d3
joonhwan.K <with.joonk@gmail.com>
2025-08-19 10:48:39 +0900

    [Silver IV] Title: ATM, Time: 72 ms, Memory: 11848 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -2,38 +2,42 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import java.util.StringTokenizer;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        List<Integer> arr = new ArrayList<>();
-
-        int min_sum = 0;
-        int cumsum_time = 0;
-
-        // arr
-        for (int i = 0; i < N; i++) {
-            arr.add(Integer.parseInt(st.nextToken()));
-        }
-
-        // asc
-        Collections.sort(arr);
-
-        // cumsum
-        for (int a : arr) {
-            cumsum_time += a;
-            min_sum += cumsum_time;
-        }
-
-        // res
-        System.out.println(min_sum);
-
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String[] p = br.readLine().split(" ");
+		int res = min_time_sum(p);
+		System.out.println(res);
+	}
+	
+	
+	
+	
+	
+	public static int min_time_sum(String[] arr) {
+		int answer = 0;
+		int[] int_arr = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			int_arr[i] = Integer.parseInt(arr[i]);
+		}
+		Arrays.sort(int_arr);
+		
+		for (int i = 1; i < int_arr.length; i++) {
+			int_arr[i] = int_arr[i] + int_arr[i-1];
+		}
+		for (int i = 0; i < int_arr.length; i++) {
+			answer += int_arr[i];
+		}
+		return answer;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
