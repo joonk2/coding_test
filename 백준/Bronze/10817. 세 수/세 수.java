55eab3f82f0e222a1e6db43540e86c50eba4c374
joonhwan.K <with.joonk@gmail.com>
2025-08-23 04:08:52 +0900

    [Bronze IV] Title: 세 수, Time: 64 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,22 +1,35 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int[] arr = new int[3];
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-
-        System.out.println(arr[1]);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] ABC = br.readLine().split(" ");
+		int res = solution(ABC);
+		System.out.println(res);
+	}
+	
+	
+	public static int solution(String[] ABC) {
+		int[] arr = new int[ABC.length];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(ABC[i]);
+		}
+		
+		// BubbleSort
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length-1; j++) {
+				if (arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}	
+		return arr[1];
+	}
+	
+	
+	
 }
