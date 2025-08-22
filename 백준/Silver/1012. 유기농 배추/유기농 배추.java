c1ccd518f20a03a3e43823d600b8dcc7e602a9d0
joonhwan.K <with.joonk@gmail.com>
2025-08-23 03:11:28 +0900

    [Silver II] Title: 유기농 배추, Time: 116 ms, Memory: 14892 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,80 +2,93 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import java.util.StringTokenizer;
+// DFS (stack)
+import java.util.Stack;
+
+// answer
+import java.util.ArrayList;
+
 
 public class Main {
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static int M,N,K;
-    static int[][] graph;
-    static boolean[][] v;
-
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-
-        while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            M = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-
-            graph = new int[N][M];
-            v = new boolean[N][M];
-
-
-            // 배추위치 설정
-            for (int i = 0; i < K; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-
-                // [행][열]
-                graph[y][x] = 1;
-            }
-
-            int res = 0;
-
-            // 배추밭 탐색
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (graph[i][j] == 1 && !v[i][j]) {
-                        dfs(i, j);
-                        res++;
-                    }
-                }
-            }
-
-            sb.append(res).append('\n');
-
-        }
-        System.out.println(sb);
-
-
-    }
-
-
-
-
-
-    public static void dfs(int x, int y) {
-        v[x][y] = true; // 방문처리
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                if (graph[nx][ny] == 1 && !v[nx][ny]) {
-                    dfs(nx,ny);
-                }
-            }
-        }
-    }
-
-
-
+	static int T, M, N, K;
+	static int[][] matrix;
+	static boolean[][] visited;
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc < T+1; tc++) {
+			String[] MNK = br.readLine().split(" ");
+			M = Integer.parseInt(MNK[0]);
+			N = Integer.parseInt(MNK[1]);
+			K = Integer.parseInt(MNK[2]);
+			
+			matrix = new int[N][M];
+			visited = new boolean[N][M];
+			
+			for (int i = 0; i < K; i++) {
+				String[] rc = br.readLine().split(" ");
+				int c = Integer.parseInt(rc[0]);
+				int r = Integer.parseInt(rc[1]);
+				matrix[r][c] = 1;
+			}
+			DFS();
+		}
+	}
+	
+	
+	
+	
+	public static void DFS() {
+		ArrayList<Integer> answer_lst = new ArrayList<>();
+		
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				if (matrix[r][c] == 1 && !visited[r][c]) {
+					int cnt = worm_cnt(r, c);
+					answer_lst.add(cnt);
+				}
+			}
+		}
+		int cnt = 0;
+		for (int i = 0; i < answer_lst.size(); i++) cnt++;
+		System.out.println(cnt);
+	}
+	
+	
+	
+	
+	
+	public static int worm_cnt(int r, int c) {
+		Stack<int[]> stack = new Stack<>();
+		stack.push(new int[] {r, c});
+		visited[r][c] = true;
+		int cnt = 1;
+		while (!stack.isEmpty()) {
+			int[] cur_pos = stack.pop();
+			int cur_r = cur_pos[0];
+			int cur_c = cur_pos[1];
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = cur_r + dr[d];
+				int nc = cur_c + dc[d];
+				if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
+					if (matrix[nr][nc] == 1 && !visited[nr][nc]) {
+						stack.push(new int[] {nr, nc});
+						visited[nr][nc] = true;
+						cnt++;
+					}
+				}
+			}
+			
+		}
+		return cnt;
+		
+	}
+	
+	
+	
 }
