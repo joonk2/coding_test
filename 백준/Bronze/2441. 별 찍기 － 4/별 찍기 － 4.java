43ed7b088429888dbccb66ce2512c6fb87d54887
joonhwan.K <with.joonk@gmail.com>
2025-08-23 04:19:33 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 212 ms, Memory: 12984 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -1,28 +1,20 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            
-            
-            for (int k = 0; k < i; k++) {
-                System.out.print(" ");
-            }
-
-            
-            for (int j = 0; j < N-i; j++) {
-                System.out.print("*");
-            }
-
-            
-            System.out.println();
-
-
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
 }
