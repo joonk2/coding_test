00841142b0efe7b7320c8312c6bf0f1b7dec2ad7
joonhwan.K <with.joonk@gmail.com>
2025-08-24 22:47:23 +0900

    [Silver I] Title: 안전 영역, Time: 244 ms, Memory: 36768 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 // DFS
 import java.util.Stack;
@@ -9,23 +9,21 @@ import java.util.Stack;
 
 public class Main {
 	static int N;
-	static int[][] map;
-	static boolean[][] visited;
 	static int[] dr = {-1,1,0,0};
 	static int[] dc = {0,0,-1,1};
-	static int max_cnt;
-	
+	static boolean[][] visited;
+	static int[][] matrix;
 	static int max_val = Integer.MIN_VALUE;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
+		matrix = new int[N][N];
 		for (int r = 0; r < N; r++) {
 			String[] cols = br.readLine().split(" ");
 			for (int c = 0; c < N; c++) {
-				map[r][c] = Integer.parseInt(cols[c]);
-				if (max_val < map[r][c]) max_val = map[r][c];
+				matrix[r][c] = Integer.parseInt(cols[c]);
+				if (matrix[r][c] > max_val) max_val = matrix[r][c];
 			}
 		}
 		DFS();
@@ -33,24 +31,26 @@ public class Main {
 	
 	
 	
+	
 	public static void DFS() {
-		max_cnt = 0;
-		for (int height = -1; height < max_val + 1; height++) {
+		int max_cnt = 0;
+		for (int height = 0; height < max_val + 1; height++) {
 			visited = new boolean[N][N];
-			int cnt = 0;
+			int temp_cnt = 0;
 			for (int r = 0; r < N; r++) {
 				for (int c = 0; c < N; c++) {
-					if (!visited[r][c] && map[r][c] > height) {
+					if (!visited[r][c] && matrix[r][c] > height) {
 						visited[r][c] = true;
 						int lets_see = safe_area_cnt(r, c, height);
-						cnt++;
+						temp_cnt++;
 					}
 				}
 			}
-			if (cnt > max_cnt) max_cnt = cnt;
+			if (temp_cnt > max_cnt) max_cnt = temp_cnt;
 		}
 		System.out.println(max_cnt);
 	}
+	
 	
 	
 	
@@ -63,12 +63,11 @@ public class Main {
 			int[] cur_pos = stack.pop();
 			int cur_r = cur_pos[0];
 			int cur_c = cur_pos[1];
-			
 			for (int d = 0; d < 4; d++) {
 				int nr = cur_r + dr[d];
 				int nc = cur_c + dc[d];
 				if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
-					if (!visited[nr][nc] && map[nr][nc] > height) {
+					if (!visited[nr][nc] && matrix[nr][nc] > height) {
 						stack.push(new int[] {nr, nc});
 						visited[nr][nc] = true;
 						cnt++;
@@ -78,8 +77,6 @@ public class Main {
 		}
 		return cnt;
 	}
-	
-	
 	
 	
 }
