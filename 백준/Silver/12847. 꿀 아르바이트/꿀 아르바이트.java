f7349c1c289259d64feca02976e693ef6591627a
joonhwan.K <with.joonk@gmail.com>
2025-08-24 19:46:41 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 212 ms, Memory: 27300 KB -BaekjoonHub

--- 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -12,25 +12,27 @@ public class Main {
 	}
 	
 	
-	
 	public static long what_is_max_profit(String[] nm, String[] arr) {
 		int n = Integer.parseInt(nm[0]);
 		int m = Integer.parseInt(nm[1]);
-		int[] int_arr = new int[n];
+		int[] wage_lst = new int[n];
 		for (int i = 0; i < n; i++) {
-			int_arr[i] = Integer.parseInt(arr[i]);
+			wage_lst[i] = Integer.parseInt(arr[i]);
 		}
 		
 		long max_profit = Long.MIN_VALUE;
 		long temp_sum = 0;
-		for (int start = 0; start < n; start++) {
-			temp_sum += int_arr[start];
-			if (start >= m-1) {
-				if (temp_sum > max_profit) {
-					max_profit = temp_sum;
-				}
-				temp_sum -= int_arr[start - (m-1)];
+		int start = 0;
+		int end = 0;
+		
+		while (end < n) {
+			temp_sum += wage_lst[end];
+			if (end-start + 1 == m) {
+				if (temp_sum > max_profit) max_profit = temp_sum;
+				temp_sum -= wage_lst[start];
+				start++;
 			}
+			end++;
 		}
 		return max_profit;
 	}
