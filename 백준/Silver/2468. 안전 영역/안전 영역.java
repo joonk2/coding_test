a66853e4c8fb02ce5594f71611088c5293317a2a
joonhwan.K <with.joonk@gmail.com>
2025-08-24 22:19:36 +0900

    [Silver I] Title: 안전 영역, Time: 252 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,73 +1,85 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+// DFS
+import java.util.Stack;
+
+
 
 public class Main {
-    static int N;
-    static int[][] graph;
-    static boolean[][] v;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        graph = new int[N][N];
-        int max_height = 0;
-
-        // 그래프 입력 및 최대 높이 찾기
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                max_height = Math.max(max_height, graph[i][j]);
-            }
-        }
-
-        int max_safe_zone = 0;
-        // 각 높이마다 안전 영역 탐색
-        for (int height = 0; height <= max_height; height++) {
-            v = new boolean[N][N];
-            int cnt_safezone = 0;
-
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    // 안전 영역 탐색 시작
-                    if (!v[i][j] && graph[i][j] > height) {
-                        dfs(i, j, height);
-                        cnt_safezone++;
-                    }
-                }
-            }
-
-            max_safe_zone = Math.max(max_safe_zone, cnt_safezone);
-        }
-        System.out.println(max_safe_zone);
-
-
-    // main 메서드 닫는 괄호
-    }
-
-
-
-
-
-    // DFS 함수
-    static void dfs(int x, int y, int height) {
-        v[x][y] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && ny >= 0 && nx < N && ny < N && !v[nx][ny] && graph[nx][ny] > height) {
-                dfs(nx, ny, height);
-            }
-        }
-    }
-
-
-
-// class 닫는 괄호
+	static int N;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
+	static int max_cnt;
+	
+	static int max_val = Integer.MIN_VALUE;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		for (int r = 0; r < N; r++) {
+			String[] cols = br.readLine().split(" ");
+			for (int c = 0; c < N; c++) {
+				map[r][c] = Integer.parseInt(cols[c]);
+				if (max_val < map[r][c]) max_val = map[r][c];
+			}
+		}
+		DFS();
+	}
+	
+	
+	
+	public static void DFS() {
+		max_cnt = 0;
+		for (int height = -1; height < max_val + 1; height++) {
+			visited = new boolean[N][N];
+			int cnt = 0;
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					if (!visited[r][c] && map[r][c] > height) {
+						visited[r][c] = true;
+						int lets_see = safe_area_cnt(r, c, height);
+						cnt++;
+					}
+				}
+			}
+			if (cnt > max_cnt) max_cnt = cnt;
+		}
+		System.out.println(max_cnt);
+	}
+	
+	
+	
+	public static int safe_area_cnt(int r, int c, int height) {
+		Stack<int[]> stack = new Stack<>();
+		stack.push(new int[] {r, c});
+		
+		int cnt = 1;
+		while (!stack.isEmpty()) {
+			int[] cur_pos = stack.pop();
+			int cur_r = cur_pos[0];
+			int cur_c = cur_pos[1];
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = cur_r + dr[d];
+				int nc = cur_c + dc[d];
+				if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
+					if (!visited[nr][nc] && map[nr][nc] > height) {
+						stack.push(new int[] {nr, nc});
+						visited[nr][nc] = true;
+						cnt++;
+					}
+				}
+			}
+		}
+		return cnt;
+	}
+	
+	
+	
+	
 }
