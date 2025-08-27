3d36a24e76e11ee56ff9b88ef8dacea82e52cbdb
joonhwan.K <with.joonk@gmail.com>
2025-08-27 22:49:51 +0900

    [Gold IV] Title: 테트로미노, Time: 480 ms, Memory: 34764 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,83 +1,103 @@
 /*
-[시도한 방법]
-1. 모든 모양을 다 그려주고 delta 탐색 진행 --> (실패)
-2. delta 탐색 X
-(테트로미노는 4방향 탐색이 아니라 미리 정의된 상대 좌표를 따라 이동해야 하므로 dx, dy를 사용불가)
-
-
-
+현재 순회중인 좌표를 temp_sum에 더한 상태로 N*M 횟수만큼 탐색 시작
+모양에 해당하는 인접블록을 3개 더하며 최댓값을 갱신
 */
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int[][][] TETROMINOS = {
-        // I 모양
-        {{0,0}, {0,1}, {0,2}, {0,3}}, {{0,0}, {1,0}, {2,0}, {3,0}},
-        // O 모양
-        {{0,0}, {0,1}, {1,0}, {1,1}},
-        // L 모양
-        {{0,0}, {1,0}, {2,0}, {2,1}}, {{0,0}, {0,1}, {0,2}, {1,0}}, 
-        {{0,0}, {0,1}, {1,1}, {2,1}}, {{0,2}, {1,0}, {1,1}, {1,2}}, 
-        {{0,0}, {0,1}, {1,0}, {2,0}}, {{0,0}, {1,0}, {1,1}, {1,2}}, 
-        {{0,1}, {1,1}, {2,0}, {2,1}}, {{0,0}, {0,1}, {0,2}, {1,2}}, 
-        // Z 모양
-        {{0,0}, {0,1}, {1,1}, {1,2}}, {{0,1}, {1,0}, {1,1}, {2,0}}, 
-        {{0,1}, {0,2}, {1,0}, {1,1}}, {{0,0}, {1,0}, {1,1}, {2,1}}, 
-        // T 모양
-        {{0,0}, {0,1}, {0,2}, {1,1}}, {{0,0}, {1,0}, {1,1}, {2,0}}, 
-        {{0,1}, {1,0}, {1,1}, {1,2}}, {{0,1}, {1,0}, {1,1}, {2,1}}
-    };
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // 첫 줄 입력
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
-
-        // 보드 입력
-        int[][] board = new int[N][M];
-        for (int y = 0; y < N; y++) {
-            String[] row = br.readLine().split(" ");
-            for (int x = 0; x < M; x++) {
-                board[y][x] = Integer.parseInt(row[x]);
-            }
-        }
-
-        int maxSum = 0;
-
-        // 모든 위치에서 테트로미노 배치 시도
-        for (int y = 0; y < N; y++) {
-            for (int x = 0; x < M; x++) {
-                for (int[][] tetromino : TETROMINOS) {
-                    int currentSum = 0;
-                    boolean valid = true;
-
-                    for (int[] pos : tetromino) {
-                        int nx = x + pos[0];
-                        int ny = y + pos[1];
- 
-                        if (ny < 0 || ny > N-1 || nx < 0 || nx > M-1) {
-                            valid = false;
-                            break;
-                        }
-                        currentSum += board[ny][nx];
-                    }
-
-                    if (valid == true) {
-                    	maxSum = Math.max(maxSum, currentSum);
-                    }
-                
-                
-                }
-            }
-        }
-
-        System.out.println(maxSum);
-    }
+	static int N, M;
+	static int[][] matrix;
+	
+	static int[][][] tetrominos = {
+		// L
+		{ {0,-1}, {-1,0}, {-2,0} },
+		{ {0,1}, {-1,0}, {-2,0} },
+		{ {0,1}, {1,0}, {2,0} },
+		{ {0,-1}, {1,0}, {2,0} },
+		
+		// ㄱ
+		{ {0,1}, {0,2}, {1,0} },
+		{ {-1,0}, {0,1}, {0,2} },
+		{ {1,0}, {0,-1}, {0,-2} },
+		{ {-1,0}, {0,-1}, {0,-2} },
+		
+		// ㅡ
+		{ {0,1}, {0,2}, {0,3} },
+		
+		// ㅣ
+		{ {1,0}, {2,0}, {3,0} },
+		
+		// ㅁ
+		{ {0,1}, {1,0}, {1,1} },
+		
+		// 번개
+		{ {0,-1}, {-1,-1}, {-1,-2} },
+		{ {0,1}, {-1,1}, {-1,2} },
+		{ {1,0}, {1,-1}, {2,-1} },
+		{ {1,0}, {1,1}, {2,1} },
+		
+		// ㅜ
+		{ {0,-1}, {0,-2}, {-1,-1} },
+		{ {-1,0}, {-2,0}, {-1,1} },
+		{ {0,1}, {0,2}, {1,1} },
+		{ {1,0}, {2,0}, {1,-1} },
+	};
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] NM = br.readLine().split(" ");
+		N = Integer.parseInt(NM[0]);
+		M = Integer.parseInt(NM[1]);
+		matrix = new int[N][M];
+		for (int r = 0; r < N; r++) {
+			String[] cols = br.readLine().split(" ");
+			for (int c = 0; c < M; c++) {
+				matrix[r][c] = Integer.parseInt(cols[c]);
+			}
+		}
+		max_sum();
+	}
+	
+	
+	
+	
+	public static void max_sum() {
+		int max_val = Integer.MIN_VALUE;
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				int temp_val = check(r, c);
+				if (temp_val > max_val) max_val = temp_val;
+			}
+		}
+		System.out.println(max_val);
+	}
+	
+	
+	public static int check(int r, int c) {
+		int max_sum = matrix[r][c];
+		for (int[][] shape : tetrominos) {
+			int temp_val = matrix[r][c];
+			for (int[] pos : shape) {
+				int dr = pos[0];
+				int dc = pos[1];
+				int nr = dr + r;
+				int nc = dc + c;
+				if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
+					temp_val += matrix[nr][nc];
+				}
+				else {
+					break;
+				}
+			}
+			if (temp_val > max_sum) max_sum = temp_val;
+		}
+		return max_sum;
+	}
+	
+	
 }
