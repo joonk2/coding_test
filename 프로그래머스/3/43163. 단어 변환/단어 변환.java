8ff62de6a01f9896e8d41e2bfe93f7b3a341f473
joonhwan.K <with.joonk@gmail.com>
2025-09-19 16:32:25 +0900

    [level 3] Title: 단어 변환, Time: 0.19 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,9 +1,10 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Arrays;
 
 class Solution {
-    static int N;
     static boolean[] visited;
+    static int N;
     
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
@@ -13,15 +14,13 @@ class Solution {
     
     
     static int bfs(String begin, String target, String[] words) {
+        Queue<int[]> q = new LinkedList<>();
         N = words.length;
         visited = new boolean[N];
         
-        Queue<int[]> q = new LinkedList<>();
-        
-        
-        // 최초 1회 알파벳 1개개 다른 단어 q에 삽입
+        // 1글자 다른 단어를 q에 최초 1회 삽입
         for (int i = 0; i < N; i++) {
-            if (!visited[i] && diff(begin, words[i])) {
+            if (!visited[i] && if_one_word_is_different(begin, words[i])) {
                 q.add(new int[] {i, 1});
                 visited[i] = true;
             }
@@ -33,27 +32,28 @@ class Solution {
             int idx = cur[0];
             int dist = cur[1];
             
-            // 가지치기
             if (words[idx].equals(target)) {
                 return dist;
             }
             
             for (int i = 0; i < N; i++) {
-                if (!visited[i] && diff(words[idx], words[i])) {
+                if (!visited[i] && if_one_word_is_different(words[idx], words[i])) {
                     q.add(new int[] {i, dist+1});
                     visited[i] = true;
                 }
             }
-            
+                
         }
         
         
-        return 0;
+     return 0;   
     }
     
     
     
-    static boolean diff(String s1, String s2) {
+    
+    
+    static boolean if_one_word_is_different(String s1, String s2) {
         int temp_cnt = 0;
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
@@ -63,8 +63,10 @@ class Solution {
                 return false;
             }
         }
+        
         return true;
     }
+    
     
     
 }
