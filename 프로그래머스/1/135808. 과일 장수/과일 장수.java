261b70257c4c3bc865cb9158bee77f0bf76043a7
joonhwan.K <with.joonk@gmail.com>
2025-09-20 14:05:17 +0900

    [level 1] Title: 과일 장수, Time: 80.31 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,24 +4,11 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         Arrays.sort(score);
-        answer = what_is_max_profit(k, m, score);
+        for (int i = score.length; i >= m; i -= m) {
+            if (i-m >= 0) {
+                answer += (score[i-m] * m);
+            }
+        }
         return answer;
     }
-    
-    
-    static int what_is_max_profit(int k, int m, int[] score) {
-        int T = score.length / m;
-        int res = 0;
-        int N = score.length-1;
-        int min_idx = N-m+1;
-        while (T > 0) {
-            res += (score[min_idx] * m);
-            N -= m;
-            min_idx -= m;
-            T--;
-        }
-        return res;
-    }
-    
-    
 }
