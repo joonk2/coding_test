671beca581bb6de868676c2400538a946c7b4f9c
joonhwan.K <with.joonk@gmail.com>
2025-09-20 21:39:07 +0900

    Update DFS와 BFS.java

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,19 +2,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
+// graph
 import java.util.ArrayList;
+
+// asc
 import java.util.Collections;
 
+// DFS
 import java.util.Stack;
 import java.util.List;
 
+// BFS
 import java.util.Queue;
 import java.util.LinkedList;
 
+// test
+import java.util.Arrays;
+
 
 public class Main {
-	static boolean[] visited;
+	// graph
 	static ArrayList<Integer>[] graph;
+	// visited
+	static boolean[] visited;
+	
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,12 +34,11 @@ public class Main {
 		int M = Integer.parseInt(NMV[1]);
 		int V = Integer.parseInt(NMV[2]);
 		
-		
+		// allocating size of graph
 		graph = new ArrayList[N+1];
 		for (int i = 1; i < N+1; i++) {
 			graph[i] = new ArrayList<>();
 		}
-		
 		
 		for (int i = 0; i < M; i++) {
 			String[] ab = br.readLine().split(" ");
@@ -38,13 +48,10 @@ public class Main {
 			graph[b].add(a);
 		}
 		
+		// asc
 		for (int i = 1; i < N+1; i++) {
 			Collections.sort(graph[i]);
 		}
-		
-//		for (int i = 1; i < N+1; i++) {
-//			System.out.println(i + " -> " + graph[i]);
-//		}
 		
 		visited = new boolean[N+1];
 		DFS(V);
@@ -58,56 +65,54 @@ public class Main {
 	
 	
 	
-	
-	
-	
-	public static void DFS(int start) {
+	static void DFS(int start_node) {
 		Stack<Integer> stack = new Stack<>();
-		stack.push(start);
+		stack.push(start_node);
 		
 		while (!stack.isEmpty()) {
-			int node = stack.pop();
-			
-			if (visited[node]) continue;
-			visited[node] = true;
-			System.out.print(node + " ");
-			
-			List<Integer> neighbors = graph[node];
-			for (int i = neighbors.size()-1; i >= 0; i--) {
-				int next = neighbors.get(i);
-				if (!visited[next]) {
-					stack.push(next);
+			int cur_node = stack.pop();
+			if (!visited[cur_node]) {
+				visited[cur_node] = true;
+				System.out.print(cur_node + " ");
+				
+				ArrayList<Integer> neighbors = graph[cur_node];
+				for (int i = neighbors.size()-1; i >= 0; i--) {
+					int next_node = neighbors.get(i);
+					if(!visited[next_node]) {
+						stack.push(next_node);
+					}
 				}
+				
 			}
 		}
-	}
-	
-	
-	
-	
-	
-	public static void BFS(int start) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.offer(start);
-		visited[start] = true;
 		
-		while (!queue.isEmpty()) {
-			int node = queue.poll();
-			System.out.print(node + " ");
-			
-			for (int next : graph[node]) {
-				if (!visited[next]) {
-					visited[next] = true;
-					queue.offer(next);
-				}
-			}
-		}
-			
-			
 	}
 	
 	
 	
+	
+	static void BFS(int start_node) {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(start_node);
+		visited[start_node] = true;
+		
+		while (!q.isEmpty()) {
+			int cur_node = q.poll();
+			System.out.print(cur_node + " ");
+			
+			ArrayList<Integer> neighbors = graph[cur_node];
+			for (int next_node : neighbors) {
+				if (!visited[next_node]) {
+					visited[next_node] = true;
+					q.offer(next_node);
+				}
+			}
+			
+		}
+		
+		
+		
+	}
 	
 	
 	
