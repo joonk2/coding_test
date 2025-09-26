f84d77c3e052cd43b62f401d27452286a7f03fca
joonhwan.K <with.joonk@gmail.com>
2025-09-27 01:55:59 +0900

    [Silver III] Title: Yonsei TOTO, Time: 80 ms, Memory: 12952 KB -BaekjoonHub

--- 백준/Silver/12018. Yonsei TOTO/Yonsei TOTO.java
+++ 백준/Silver/12018. Yonsei TOTO/Yonsei TOTO.java
@@ -1,75 +1,45 @@
-/*
-우선 정원미달일 경우 최소 등록 마일리지는 1,
-
-필요한 마일리지는
-25, 20, 1, 14
-36은 마일리지 모자라서 못쓴다
- */
-
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] firstLine = br.readLine().trim().split(" ");
-        int n = Integer.parseInt(firstLine[0]);
-        int m = Integer.parseInt(firstLine[1]);
-
-        ArrayList<Integer> li = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            String[] secondLine = br.readLine().trim().split(" ");
-            int P = Integer.parseInt(secondLine[0]);
-            int L = Integer.parseInt(secondLine[1]);
-
-            String[] point_arr = br.readLine().trim().split(" ");
-            int[] point = new int[P];
-            for (int j = 0; j < P; j++) {
-                point[j] = Integer.parseInt(point_arr[j]);
-            }
-
-            // asc;
-            Arrays.sort(point);
-
-            int threshold;
-            if (P >= L) {
-                threshold = point[P-L];
-            }
-            else {
-                threshold = 1;
-            }
-
-            li.add(threshold);
-
-
-
-        }
-
-        // asc
-        Collections.sort(li);
-
-        int cnt = 0;
-        for (int i : li) {
-            if (m-i >= 0) {
-                cnt++;
-                m -= i;
-            }
-            else {
-                break;
-            }
-        }
-
-
-        System.out.println(cnt);
-
-
-
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] N_mileage = br.readLine().split(" ");
+		int N = Integer.parseInt(N_mileage[0]);
+		int mileage = Integer.parseInt(N_mileage[1]);
+		
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			String[] P_L = br.readLine().split(" ");
+			String[] score_arr = br.readLine().split(" ");
+			int P = Integer.parseInt(P_L[0]);
+			int L = Integer.parseInt(P_L[1]);
+			int[] score = new int[P];
+			for (int j = 0; j < P; j++) {
+				score[j] = Integer.parseInt(score_arr[j]);
+			}
+			Arrays.sort(score);
+			
+			if (P >= L) {
+				arr[i] = score[P-L];
+			}
+			else if (P < L) {
+				arr[i] = 1;
+			}
+		}
+		Arrays.sort(arr);
+		
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			if (mileage - arr[i] >= 0) {
+				mileage -= arr[i];
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		
+	}
 }
