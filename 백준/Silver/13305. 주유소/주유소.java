1460791d0b02b3ec6d5a24cbf4faf3895aeffb40
joonhwan.K <with.joonk@gmail.com>
2025-10-04 20:28:25 +0900

    [Silver III] Title: 주유소, Time: 340 ms, Memory: 44512 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -4,19 +4,16 @@ import java.io.InputStreamReader;
 
 public class Main {
 	static int[] dist, price;
-	static int min_cost = Integer.MAX_VALUE;
-	static int total_dist = 0;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		String[] dist_arr = br.readLine().split(" ");
 		String[] price_arr = br.readLine().split(" ");
-		dist = new int[dist_arr.length + 1];
+		dist = new int[dist_arr.length];
 		price = new int[N];
-		for (int i = 0; i < dist.length-1; i++) {
+		for (int i = 0; i < dist.length; i++) {
 			dist[i] = Integer.parseInt(dist_arr[i]);
-			total_dist += dist[i];
 		}
 		for (int i = 0; i < price.length; i++) {
 			price[i] = Integer.parseInt(price_arr[i]);
@@ -29,17 +26,13 @@ public class Main {
 	
 	
 	static void what_is_the_answer() {
-		min_cost = dist[0] * price[0];
-		total_dist -= dist[0];
-		for (int i = 1; i < dist.length-1; i++) {
-			if (price[i] <= price[i+1]) {
-				if (total_dist >= 0) {
-					min_cost += (price[i] * total_dist);
-					total_dist = 0;
-				}
-			}
+		long total_cost = 0;
+		long min_price = price[0];
+		for (int i = 0; i < price.length-1; i++) {
+			min_price = Math.min(price[i], min_price);
+			total_cost += (dist[i] * min_price);
 		}
-		System.out.println(min_cost);
+		System.out.println(total_cost);
 	}
 	
 	
