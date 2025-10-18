878b8e10a619bc6f83d59463e1f153916c15ef59
joonhwan.K <with.joonk@gmail.com>
2025-10-18 21:18:46 +0900

    [level 3] Title: 입국심사, Time: 115.95 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        long left = 1;
+        long right = (long) times[times.length-1]*n;
+        long res = 0;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            if (higher_than_n(mid, times, n)) {
+                res = mid;
+                right = mid - 1;
+            } 
+            else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+    
+    
+    static boolean higher_than_n(long mid, int[] times, int n) {
+        long cnt = 0;
+        for (int i = 0; i < times.length; i++) {
+            cnt += (mid / times[i]);
+        }
+        if (cnt >= n) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+}
