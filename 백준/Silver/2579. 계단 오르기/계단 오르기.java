016fb5ab5a26873f800f783ef17709a95a552221
joonhwan.K <with.joonk@gmail.com>
2025-12-16 00:35:10 +0900

    [Silver III] Title: 계단 오르기, Time: 68 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -5,13 +5,13 @@ import java.io.BufferedReader;
 public class Main {
 	static int N;
 	static int[] arr;
-	static int[] DP;
+	static int[][] DP;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N+1];
-		DP = new int[N+1];
+		DP = new int[N+1][3];
 		for (int i = 0; i < N; i++) {
 			int num = Integer.parseInt(br.readLine());
 			arr[i+1] = num;
@@ -24,25 +24,25 @@ public class Main {
 	
 	
 	static void lets_find_max_val() {
-		if (N == 1) {
-			System.out.println(arr[1]);
-			return;
-		}
-		if (N == 2) {
-			System.out.println(arr[1] + arr[2]);
-			return;
+		// 첫계단
+		DP[1][1] = arr[1];
+		
+		// 두번째 계단
+		// [1] 전계단 안 밟음
+		// [2] 전계단 밟음
+		if (N >= 2) {
+			DP[2][1] = arr[2];
+			DP[2][2] = arr[1] + arr[2];
 		}
 		
-		DP[1] = arr[1];
-		DP[2] = arr[1] + arr[2];
-		DP[3] = Math.max(arr[1] + arr[3], arr[2] + arr[3]);
-		for (int i = 4; i < N+1; i++) {
-			DP[i] = Math.max(DP[i-2] + arr[i], DP[i-3] + arr[i-1] + arr[i]);
+		for (int i = 3; i < N+1; i++) {
+			DP[i][1] = Math.max(DP[i-2][1], DP[i-2][2]) + arr[i];
+			DP[i][2] = DP[i-1][1] + arr[i];
 		}
-		System.out.println(DP[N]);
+		
+		
+		System.out.println(Math.max(DP[N][1], DP[N][2]));
 	}
-	
-	
 	
 	
 }
