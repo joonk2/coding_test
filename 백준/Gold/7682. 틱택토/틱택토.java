a0fc3dc6cdc001b63d355afd7ceeed1cdf493554
joonhwan.K <with.joonk@gmail.com>
2025-12-29 18:39:27 +0900

    [Gold V] Title: 틱택토, Time: 68 ms, Memory: 11364 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.java
+++ 백준/Gold/7682. 틱택토/틱택토.java
@@ -1,62 +1,49 @@
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 
 public class Main {
-	static boolean win(char[] b, char c) {
-		int[][] lines = {
-				{0,1,2}, {3,4,5}, {6,7,8},
-				{0,3,6}, {1,4,7}, {2,5,8},
-				{0,4,8}, {2,4,6}
-		};
-		
-		for (int[] line : lines) {
-			if (b[line[0]] == c && b[line[1]] == c && b[line[2]] == c) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	
-	
+	static int[][] lines = {
+			{0,1,2}, {3,4,5}, {6,7,8},
+			{0,3,6}, {1,4,7}, {2,5,8},
+			{0,4,8}, {2,4,6}
+	};
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			String str = br.readLine();
 			if (str.equals("end")) break;
-			char[] board = str.toCharArray();
+			char[] arr = str.toCharArray();
 			
-			int x = 0;
-			int o = 0;
-			for (char c : board) {
-				if (c == 'X') x++;
-				if (c == 'O') o++;
+			int cnt_X = 0;
+			int cnt_O = 0;
+			for (int i = 0; i < 9; i++) {
+				if (arr[i] == 'X') cnt_X++;
+				if (arr[i] == 'O') cnt_O++;
 			}
 			
-			boolean x_win = win(board, 'X');
-			boolean o_win = win(board, 'O');
-			
 			boolean valid = true;
+			boolean win_X = win(arr, 'X');
+			boolean win_O = win(arr, 'O');
 			
-			// 개수 규칙 -> x가 선공이니 더 많아야한다
-			if (!(x == o || x == o+1)) valid = false;
+
 			
-			// 둘다 이기는 경우
-			if (x_win && o_win) valid = false;
+			// 게임 규칙 (갯수가 같거나 || X 갯수 > O 갯수)
+			if ( !(cnt_X == cnt_O || cnt_X == cnt_O + 1) ) valid = false;
 			
-			// x가 이겼는데 개수가 안맞다
-			if (x_win && x != o+1) valid = false;
+			// 둘다 안이겼는데 게임이 안 끝났을 때 -> .(놓을 수 있는 곳) 남았을 떄
+			if (!win_X && !win_O && cnt_X + cnt_O != 9) valid = false;
 			
-			// o가 이겼는데 개수가 안맞다
-			if (o_win && x != o) valid = false;
+			// X가 이겼는데 돌 갯수가 안 맞을 때
+			if (win_X && cnt_X != cnt_O + 1) valid = false;
 			
-			// 아무도 안 이겼는데 판이 안찼다
-			if (!x_win && !o_win && x + o != 9) valid = false;
+			// O가 이겼는데 돌 갯수가 안 맞을 때
+			if (win_O && cnt_O != cnt_X) valid = false;
 			
 			
+			// ------------- 결과 -----------------
 			if (valid) {
 				System.out.println("valid");
 			}
@@ -66,59 +53,14 @@ public class Main {
 	
 	
 	
-	
-	static void lets_figure_out_the_answer(char[] arr) {
-		// 1번째 요소가 X가 아니라면 invalid
-		if (arr[0] != 'X') {
-			System.out.println("invalid");
-			return;
-		}
-		
-		int idx_circle = 999;
-		int idx_point = 999;
-		for (int i = 1; i < 9; i++) {
-			if (arr[i] == 'O' && idx_circle == 999) {
-				idx_circle = i;
-			}
-			if (arr[i] == '.' && idx_point == 999) {
-				idx_point = i;
+	static boolean win(char[] arr, char ch) {
+		for (int[] line : lines) {
+			if (arr[line[0]] == ch && arr[line[1]] == ch && arr[line[2]] == ch) {
+				return true;
 			}
 		}
-		
-		// 만약 .이 O보다 먼저 나오면 invalid
-		if (idx_point < idx_circle) {
-			System.out.println("invalid");
-			return;
-		}
-	
-		
-		
-		// 판별 알고리즘
-		for (int i = 0; i < 9; i++) {
-			// 왼위 -> 오아래
-			if (i == 0 && arr[0] == arr[4] && arr[4] == arr[8] && arr[8] == arr[0]) {
-				System.out.println("valid");
-				return;
-			}
-			// 위에서 아래
-			if (i < 9 && i + 3 < 9 && i + 6 < 9) {
-				if (arr[i] == arr[i+3] && arr[i+3] == arr[i+6] && arr[i+6] == arr[i]) {
-					System.out.println("valid");
-					return;
-				}
-			}
-			// 오위 -> 왼아래
-			if (i == 2 && arr[2] == arr[4] && arr[4] == arr[6] && arr[6] == arr[2]) {
-				System.out.println("valid");
-				return;
-			}
-			// 직선 
-		}
-		
-		
+		return false;
 	}
-	
-	
 	
 	
 	
