b2be1bef22d3aefef7be7246a51cc84609e3279d
joonhwan.K <with.joonk@gmail.com>
2025-12-31 10:12:51 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 64 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
@@ -2,57 +2,68 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+
 public class Main {
 	static int[] dr = {1,1,1};
 	static int[] dc = {-1,0,1};
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] RC = br.readLine().split(" ");
-		int R = Integer.parseInt(RC[0]);
-		int C = Integer.parseInt(RC[1]);
-		int[][] arr = new int[R][C];
-		for (int r = 0; r < R; r++) {
+		String[] NM = br.readLine().split(" ");
+		int N = Integer.parseInt(NM[0]);
+		int M = Integer.parseInt(NM[1]);
+		int[][] arr = new int[N][M];
+		for (int r = 0; r < N; r++) {
 			String[] cols = br.readLine().split(" ");
-			for (int c = 0; c < C; c++) {
+			for (int c = 0; c < M; c++) {
 				arr[r][c] = Integer.parseInt(cols[c]);
 			}
 		}
-		min_fuel(arr, R, C);
+		lets_figure_out_min_fuel(N, M, arr);
 	}
 	
 	
-	static void min_fuel(int[][] arr, int R, int C) {
-		int INF = Integer.MAX_VALUE;
-		long[][][] DP = new long[R][C][3];
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
+	
+	
+	static void lets_figure_out_min_fuel(int N, int M, int[][] arr) {
+		int INF = 1_000_000;
+		int[][][] DP = new int[N][M][3];
+		
+		// initiation INF
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
 				for (int d = 0; d < 3; d++) {
 					DP[r][c][d] = INF;
 				}
 			}
 		}
 		
-		// first row
-		for (int c = 0; c < C; c++) {
-			for (int d = 0; d < 3; d++) {
-				DP[0][c][d] = arr[0][c];
+		
+		
+		
+		// input first row on DP
+		for (int r = 0; r < 1; r++) {
+			for (int c = 0; c < M; c++) {
+				for (int d = 0; d < 3; d++) {
+					DP[r][c][d] = arr[r][c];
+				}
 			}
 		}
 		
+		
 		// explore
-		for (int r = 1; r < R; r++) {
-			for (int c = 0; c < C; c++) {
+		for (int r = 1; r < N; r++) {
+			for (int c = 0; c < M; c++) {
 				for (int d = 0; d < 3; d++) {
 					int prev_r = r-1;
 					int prev_c = c - dc[d];
 					
-					// pruning (reverse check)
-					if (prev_c < 0 || prev_c >= C) continue;
+					// 만약 모서리지점인데 위치를 벗어나면 skip
+					if (prev_c < 0 || prev_c >= M) continue;
+					
+					// 만약 d 랑 prev_d 같으면 skip
 					for (int prev_d = 0; prev_d < 3; prev_d++) {
-						
-						// pruning -> (prev_d != d)
-						if (prev_d == d) continue;
+						if (d == prev_d) continue;
 						DP[r][c][d] = Math.min(DP[r][c][d], DP[prev_r][prev_c][prev_d] + arr[r][c]);
 					}
 				}
@@ -60,15 +71,17 @@ public class Main {
 		}
 		
 		
-		// res (exploring the last row)
-		long min_fuel = Long.MAX_VALUE;
-		for (int c = 0; c < C; c++) {
+		// result
+		int min_fuel = INF;
+		for (int c = 0; c < M; c++) {
 			for (int d = 0; d < 3; d++) {
-				min_fuel = Math.min(min_fuel, DP[R-1][c][d]);
+				min_fuel = Math.min(min_fuel, DP[N-1][c][d]);
 			}
 		}
 		System.out.println(min_fuel);
+		
 	}
+	
 	
 	
 }
