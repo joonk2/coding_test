eb3a07a3c222e457c716fd7af096e770c6492212
joonhwan.K <with.joonk@gmail.com>
2026-01-01 17:21:33 +0900

    [Silver I] Title: RGB거리, Time: 84 ms, Memory: 16760 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -2,42 +2,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-
-        int[][] cost = new int[N][3]; // 각 집을 R,G,B 로 칠하는 비용
-        int[][] dp = new int[N][3]; // DP 테이블
-
-        // input
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            cost[i][0] = Integer.parseInt(st.nextToken());
-            cost[i][1] = Integer.parseInt(st.nextToken());
-            cost[i][2] = Integer.parseInt(st.nextToken());
-        }
-
-        // dp first_setting
-        dp[0][0] = cost[0][0];
-        dp[0][1] = cost[0][1];
-        dp[0][2] = cost[0][2];
-
-
-        // dp
-        for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i][0]; // 현재 집을 R로 칠함
-            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i][1]; // 현재 집을 G로 칠함
-            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i][2]; // 현재 집을 B로
-        }
-
-        // res --> 마지막 집에서 최소 비용
-        int res = Math.min(dp[N-1][0], Math.min(dp[N-1][1], dp[N-1][2]));
-        System.out.println(res);
-
-
-    }
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][N];
+		for (int r = 0; r < N; r++) {
+			String[] cols = br.readLine().split(" ");
+			for (int c = 0; c < 3; c++) {
+				arr[r][c] = Integer.parseInt(cols[c]);
+			}
+		}
+		lets_figure_out_min_cost(N, arr);
+	}
+	
+	
+	
+	static void lets_figure_out_min_cost(int N, int[][] arr) {
+		int[][] DP = new int[N][3];
+		DP[0][0] = arr[0][0];
+		DP[0][1] = arr[0][1];
+		DP[0][2] = arr[0][2];
+		
+		// 색깔이 겹치면 안된다 -> 각각 보자
+		for (int i = 1; i < N; i++) {
+			DP[i][0] = Math.min(DP[i-1][1], DP[i-1][2]) + arr[i][0];
+			DP[i][1] = Math.min(DP[i-1][0], DP[i-1][2]) + arr[i][1];
+			DP[i][2] = Math.min(DP[i-1][1] , DP[i-1][0]) + arr[i][2];
+		}
+		
+		int res = 1001;
+		res = Math.min(DP[N-1][0], Math.min(DP[N-1][1], DP[N-1][2]));
+		System.out.println(res);
+		
+	}
+	
+	
 }
