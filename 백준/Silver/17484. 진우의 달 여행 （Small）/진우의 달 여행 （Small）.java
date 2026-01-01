3f235c94c7c62b5ed9d10a26a8bf50047433cc6e
joonhwan.K <with.joonk@gmail.com>
2026-01-01 14:00:30 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 64 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 
 public class Main {
@@ -19,17 +19,16 @@ public class Main {
 				arr[r][c] = Integer.parseInt(cols[c]);
 			}
 		}
+		
 		lets_figure_out_min_fuel(N, M, arr);
 	}
 	
 	
 	
-	
 	static void lets_figure_out_min_fuel(int N, int M, int[][] arr) {
+		// 1. 최소거리 INF 초기화
 		int INF = 1_000_000;
 		int[][][] DP = new int[N][M][3];
-		
-		// initiation INF
 		for (int r = 0; r < N; r++) {
 			for (int c = 0; c < M; c++) {
 				for (int d = 0; d < 3; d++) {
@@ -38,10 +37,7 @@ public class Main {
 			}
 		}
 		
-		
-		
-		
-		// input first row on DP
+		// 2. 첫번째 행 DP에 입력
 		for (int r = 0; r < 1; r++) {
 			for (int c = 0; c < M; c++) {
 				for (int d = 0; d < 3; d++) {
@@ -50,18 +46,21 @@ public class Main {
 			}
 		}
 		
+		// 3. 탐험 (2번째 행부터)
+		// 3-1. 현재 위치가 범위를 벗어나는지?
+		// 3-2. 이전위치가 범위를 벗어났는지?
+		// 3-3. 현재 방향이 이전방향과 같은지?
 		
-		// explore
 		for (int r = 1; r < N; r++) {
+			// 이 반복문으로 현재 위치는 범위를 벗어나지않는 것을 확인
 			for (int c = 0; c < M; c++) {
 				for (int d = 0; d < 3; d++) {
 					int prev_r = r-1;
 					int prev_c = c - dc[d];
-					
-					// 만약 모서리지점인데 위치를 벗어나면 skip
+					// 이전 위치가 범위를 벗어나는가?
 					if (prev_c < 0 || prev_c >= M) continue;
 					
-					// 만약 d 랑 prev_d 같으면 skip
+					// 현재 방향이 이전 방향이랑 같은가?
 					for (int prev_d = 0; prev_d < 3; prev_d++) {
 						if (d == prev_d) continue;
 						DP[r][c][d] = Math.min(DP[r][c][d], DP[prev_r][prev_c][prev_d] + arr[r][c]);
@@ -70,8 +69,7 @@ public class Main {
 			}
 		}
 		
-		
-		// result
+		// 4. 결과
 		int min_fuel = INF;
 		for (int c = 0; c < M; c++) {
 			for (int d = 0; d < 3; d++) {
@@ -79,8 +77,8 @@ public class Main {
 			}
 		}
 		System.out.println(min_fuel);
-		
 	}
+	
 	
 	
 	
