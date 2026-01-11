cf6c642eea0a0ec5bdb13297d084e71b872d0287
joonhwan.K <with.joonk@gmail.com>
2026-01-11 10:21:49 +0900

    [Gold IV] Title: 지뢰찾기, Time: 60 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Gold/9082. 지뢰찾기/지뢰찾기.java
+++ 백준/Gold/9082. 지뢰찾기/지뢰찾기.java
@@ -1,5 +1,9 @@
 /*
-그리디 
+#일때는 주변숫자를 1개라도 줄일 수 있다면 지뢰 매설가능지역으로 판단
+주변 숫자들을 1이상일시 제거한다 3방향으로 반복하면서
+
+그러나 인근지역 3곳을 확인하다가 한곳이라도 0이 나오면 그곳은 break (탐색종료)
+그리고 다음 지점을 확인한다.
 */
 
 
@@ -7,73 +11,65 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-
 public class Main {
-	static char[] block;
-	static int[] numbers;
-	static int N;
-	
+	static int[] dr = {-1,0,1};
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		
 		for (int tc = 1; tc < T+1; tc++) {
-			N = Integer.parseInt(br.readLine());
-			String num_arr = br.readLine();
-			block = br.readLine().toCharArray();
-			numbers = new int[N];
-			for (int i = 0; i < N; i++) {
-				numbers[i] = num_arr.charAt(i) - '0';
-			}
+			int N = Integer.parseInt(br.readLine());
+			String nums = br.readLine();
+			int[] arr = new int[N];
+			for (int i = 0; i < N; i++) arr[i] = nums.charAt(i)-'0';
+			char[] bomb_area = br.readLine().toCharArray();
 			
-			
-			lets_find_maximum_bomb();			
+			lets_find_max_bomb(N, arr, bomb_area);
 		}
-		
-		
-		
 	}
 	
 	
 	
 	
-	
-	static void lets_find_maximum_bomb() {
-		int cnt = 0;
+	static void lets_find_max_bomb(int N, int[] arr, char[] bomb_area) {
 		for (int i = 0; i < N; i++) {
-			// 1. 지뢰일 때 놓을 수 있는지 검사
-			if (block[i] == '*') {
-				for (int j = i-1; j <= i+1; j++) {
-					if (j >= 0 && j < N) {
-						numbers[j]--;
+			boolean temp_check = false;
+			
+			
+			// 매설 되어있으면
+			if (bomb_area[i] == '*') {
+				for (int d = 0; d < 3; d++) {
+					if (i + dr[d] >= 0 && i + dr[d] < N) {
+						if (arr[i+dr[d]] < 1) break;
+						arr[ i+dr[d] ]--;
 					}
 				}
-				cnt++;
 			}
-			// 2-1. 지뢰가 아닐 때 놓을 수 있는지 검사
-			else if (block[i] == '#') {
-				boolean flag = true;
-				for (int j = i-1; j <= i+1; j++) {
-					if (j >= 0 && j < N && numbers[j] <= 0) {
-						flag = false;
-						break;
+			
+			
+			// 매설되있는지 잘 모르겠다면
+			else if (bomb_area[i] == '#') {
+				for (int d = 0; d < 3; d++) {
+					if (i + dr[d] >= 0 && i + dr[d] < N) {
+						if (arr[ i+dr[d] ] < 1) break;
+						arr[ i+dr[d] ]--;
+						temp_check = true;
 					}
 				}
-				// 2-2. 유효성 검사
-				if (flag) {
-					for (int j = i-1; j <= i+1; j++) {
-						if (j >= 0 && j < N) {
-							numbers[j]--;
-						}
-					}
-					block[i] = '*';
-					cnt++;
+				// temp_check 확인
+				if (temp_check) {
+					bomb_area[i] = '*';
 				}
 			}
 		}
-		// 3. 출력
-		System.out.println(cnt);	
+		
+		
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			if (bomb_area[i] == '*') cnt++;
+		}
+		System.out.println(cnt);
+		
 	}
 	
 	
