24ec5de56cf599b7326473015a78fc5a9f0c615a
joonhwan.K <with.joonk@gmail.com>
2026-02-04 01:00:50 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 47.33 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지 - 340212 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212) 
 
 ### 성능 요약
 
-메모리: 134 MB, 시간: 66.37 ms
+메모리: 138 MB, 시간: 47.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 09일 22:53:08
+2026년 02월 04일 01:00:46
 
 ### 문제 설명
 
--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,65 +1,66 @@
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int answer = 0;
-        answer = binary_search(diffs, times, limit);
-        return answer;
-    }
-    
-    static int binary_search(int[] diffs, int[] times, long limit) {
-        int min = 1;
-        int max = Integer.MIN_VALUE;
-        int res = 0;
-        
-        for (int i : diffs) {
-            if (i > max) max = i;
+        // 최소, 최대 구하기
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MIN_VALUE;
+        for (int i = 0; i < diffs.length; i++) {
+            if (diffs[i] < left) left = diffs[i];
+            if (diffs[i] > right) right = diffs[i];
         }
+       
+        // 이진탐색
+        int req_min_level = 0;
         
-        
-        while (min <= max) {
-            int level = (min + max) / 2;
-            if (is_possible(diffs, times, limit, level)) {
-                res = level;
-                max = level - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            // 현재 레벨로 퍼즐을 풀 수 있다면
+            if (can_solve_puzzle(diffs, times, limit, mid)) {
+                req_min_level = mid;
+                right = mid - 1;
             }
-            else if (!is_possible(diffs, times, limit, level)) {
-                min = level + 1;
+            // 현재 레벨로 퍼즐 못푼다면
+            else if (!can_solve_puzzle(diffs, times, limit, mid)) {
+                left = mid + 1;
             }
         }
-        return res;
-    }
-    
-    
-    static boolean is_possible(int[] diffs, int[] times, long limit, int level) {
-        int N = diffs.length;
-        long total_sum = 0;
-        
-        for (int i = 0; i < N; i++) {
-            int temp_sum = 0;
-        	if (diffs[i] > level) {
-                int wrong_cnt = (diffs[i] - level);
-                if (i > 0) {
-                    temp_sum = (times[i-1] + times[i]) * wrong_cnt + times[i];
-                }
-                else if (i <= 0) {
-                    temp_sum = times[i] * (wrong_cnt + 1);
-                }
-            }
-            else if (diffs[i] <= level) {
-                temp_sum = times[i];
-            }
-            total_sum += temp_sum;
-            
-            // 가지치기
-            if (total_sum > limit) break;
-        }
-        
-        if (total_sum > limit) return false;
-        
-        
-        return true;
+        return req_min_level;
     }
     
     
+    
+    
+    
+    static boolean can_solve_puzzle(int[] diffs, int[] times, long limit, int mid) {
+        long cur_solving_time = 0;
+        for (int i = 0; i < times.length; i++) {
+            int time_cur = times[i];
+            int time_prev;
+            // 1-1. 현재 레벨이 더 낮을 때
+            if (mid < diffs[i]) {
+                // 만약 첫빠따면 이전 퍼즐 0 처리
+                if (i-1 < 0) time_prev = 0;
+                // 아니라면 이전퍼즐 -> i-1
+                else time_prev = times[i-1];
+                
+                int wrong_cnt = diffs[i] - mid;
+                
+                // 계산
+                cur_solving_time += ((time_prev + time_cur) * wrong_cnt) + times[i];
+            }
+            // 1-2. 현재 레벨 이상이면
+            else if (mid >= diffs[i]) {
+                cur_solving_time += times[i];
+            }
+        }
+        
+        // 판별
+        // 2-1. 제한 이내면 solve 가능
+        if (cur_solving_time <= limit) {
+            return true;
+        }
+        // 2-2. 제한 초과면 solve 불가능
+        return false;
+    }
     
     
     
