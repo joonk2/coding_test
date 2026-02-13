84a7398d7852083b6f162556b67b062e0a2f4090
joonhwan.K <with.joonk@gmail.com>
2026-02-13 15:38:36 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,26 +1,31 @@
+/*
+[힌트]
+현재의 가동중인 서버 현황을 잘 확인하고, 최소한의 전력을 사용해야한다.
+*/
+
+
 class Solution {
     public int solution(int[] players, int m, int k) {
-        int time = 24;
         int cnt = 0;
         
-        
-        // 현재 구동중인 서버갯수의 현황
+        int time = 24;
+        // 현재 가동중인 서버 현황
         int[] DP = new int[time];
         
-        
         for (int t = 0; t < time; t++) {
+            // 현재 필요한 서버수
             int req_server = players[t] / m;
             int diff = 0;
             
-            // 서버가 부족한 상황이라면
+            // 현재 가동중인 서버 < 현재 필요한 서버수라면??
             if (DP[t] < req_server) {
                 diff = req_server - DP[t];
                 cnt += diff;
             }
             
-            // 서버 개설하기
+            // 현재 가동중인 서버에 k 만큼 전력 추가
             for (int i = t; i < t + k; i++) {
-                // 범위 밖이면 skip
+                // 범위 초과시 -> skip
                 if (i >= time) continue;
                 
                 // 그게 아니라면
@@ -28,7 +33,6 @@ class Solution {
             }
             
         }
-            
         
         
         return cnt;
