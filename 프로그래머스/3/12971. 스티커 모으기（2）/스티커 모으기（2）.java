d7d19f9f27855dbd407f7229cd92be95ce6d7587
joonhwan.K <with.joonk@gmail.com>
2026-02-22 21:56:48 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.61 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int sticker[]) {
-        int answer = 0;
+        // 2개의 배열을 생성하여 값 비교하자
         int N = sticker.length;
         
-        // 조기종료
+        // 1. 조기종료
         if (N == 1) {
             int res = sticker[0];
             return res;
@@ -14,34 +14,24 @@ class Solution {
         }
         
         
-        
+        // 2-1. 0번째 idx 선택 -> 0 ~ N-2
         int[] DP_0 = new int[N];
-        int[] DP_1 = new int[N];
-        
-        // 1. 초기값 설정
-        // 1-1. 0번째를 선택한 배열 -> 0 ~ N-2
         DP_0[0] = sticker[0];
         DP_0[1] = DP_0[0];
-        
-        // 1-2. 0번째 선택 안한 배열 ->  N-1 까지
-        DP_1[1] = sticker[1];
-        DP_1[2] = Math.max(DP_1[1], sticker[2]);
-        
-        // 2. 점화식 각각
-        
-        // 2-1. 0번째를 선택한 배열
         for (int i = 2; i < N-1; i++) {
             DP_0[i] = Math.max(DP_0[i-1], sticker[i] + DP_0[i-2]);
         }
         
-        // 2-2. 1번째부터 선택한 배열
+        // 2-2. 0번째 idx 미선택 -> ~ N-1
+        int[] DP_1 = new int[N];
+        DP_1[1] = sticker[1];
+        DP_1[2] = Math.max(DP_1[1], sticker[2]);
         for (int i = 3; i < N; i++) {
             DP_1[i] = Math.max(DP_1[i-1], sticker[i] + DP_1[i-2]);
         }
         
-        
-        // 3. 정답
-        answer = Math.max(DP_0[N-2], DP_1[N-1]);
+        // 3. 비교
+        int answer = Math.max(DP_0[N-2], DP_1[N-1]);
         return answer;
     }
 }
