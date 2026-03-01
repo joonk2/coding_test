7b8a1a59dc00b6911853ca999a4b499fb9c77d3d
joonhwan.K <with.joonk@gmail.com>
2026-03-02 00:26:09 +0900

    [level 2] Title: 방문 길이, Time: 0.13 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -6,27 +6,19 @@ class Solution {
     
     public int solution(String dirs) {
         int leng = 0;
+        
         int N = dirs.length();
         char[] commands = new char[N];
-        for (int i = 0; i < N; i++) {
-            commands[i] = dirs.charAt(i);
-        }
+        for (int i = 0; i < N; i++) commands[i] = dirs.charAt(i);
         
-
-        
-        // 1. 초기값 설정
+        // 1. 초기 시작
+        boolean[][][] visited = new boolean[10 + 1][10 + 1][4];
         int cr = 5;
         int cc = 5;
-        int cd = 0;
-        boolean[][][] visited = new boolean[10+1][10+1][4];
-        
-        // 2. 탐색
         int i = 0;
         while (i < N) {
-            // 현재 버튼
             char cur_button = commands[i];
             
-            // 방향
             int dr = 0;
             int dc = 0;
             int dd = 0;
@@ -37,7 +29,7 @@ class Solution {
                 dd = 0;
             }
             else if (cur_button == 'D') {
-                dr = 1;
+            	dr = 1;
                 dc = 0;
                 dd = 1;
             }
@@ -52,43 +44,40 @@ class Solution {
                 dd = 3;
             }
             
-            // 다음 방향
+            // 다음 좌표
             int nr = cr + dr;
             int nc = cc + dc;
             int nd = dd;
             
-            // 2-1. 범위 밖 -> 다음으로 전환후 skip
+            
+            // 2. 범위 밖 -> 다음으로 전환 후, skip
             if (nr < sr || nr > er || nc < sc || nc > ec) {
                 i++;
                 continue;
             }
             
-            // 2-2. 방문했으면 -> 위치 이동, 다음으로 전환 후 skip
-            if (visited[nr][nc][nd]) {
+            // 3. 양방향 값 확인
+            int reverse_d = 0;
+            if (nd == 0) reverse_d = 1;
+            else if (nd == 1) reverse_d = 0;
+            else if (nd == 2) reverse_d = 3;
+            else if (nd == 3) reverse_d = 2;
+            
+            // 4. 방문했다면 좌표를 갱신하고, 다음으로 전환하고 skip
+            if (visited[nr][nc][nd] || visited[cr][cc][reverse_d]) {
                 cr = nr;
                 cc = nc;
-                cd = nd;
                 i++;
                 continue;
             }
             
-            
-            // 2-3-a. 양방향 처리
-            int reversed_d = 0;
-            if (dd == 0) reversed_d = 1;
-            else if (dd == 1) reversed_d = 0;
-            else if (dd == 2) reversed_d = 3;
-            else if (dd == 3) reversed_d = 2;
-            
-            // 2-3-b. 범위O, 미방문시 -> 위치이동후 횟수++, 그리고 다음으로 전환
-            
+            // 5. 양방향 방문 후에, 좌표 갱신
             visited[nr][nc][nd] = true;
-            visited[cr][cc][reversed_d] = true;
+            visited[cr][cc][reverse_d] = true;
             
             leng++;
             cr = nr;
             cc = nc;
-            cd = nd;
             i++;
         }
         
