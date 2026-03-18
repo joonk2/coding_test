25836b19a17fc49c4062fd622393cefc77698812
joonhwan.K <with.joonk@gmail.com>
2026-03-18 22:21:00 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.09 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,21 +1,22 @@
 import java.util.Arrays;
 
 class Solution {
-    static int[] dr = {1,-1,0,0};
+    static int[] dr = {-1,1,0,0};
     static int[] dc = {0,0,-1,1};
     
-    static int[] dr_diag = {-1, -1, 1, 1};
-    static int[] dc_diag = {-1, 1, -1, 1};
+    static int[] dr_diag = {-1,-1,1,1};
+    static int[] dc_diag = {-1,1,-1,1};
     
     static int row, col;
     
-    
     public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
+        int T = places.length;
+        int[] answer = new int[T];
         
-        for (int t = 0; t < places.length; t++) {
-            row = places[0].length;
-            col = places[0][0].length();
+        for (int t = 0; t < T; t++) {
+            row = places[t].length;
+            col = places[t][0].length();
+            
             char[][] arr = new char[row][col];
             
             // 1. 배열 생성
@@ -27,31 +28,38 @@ class Solution {
             boolean flag = false;
             for (int r = 0; r < row; r++) {
                 for (int c = 0; c < col; c++) {
+                    // 2-1. 사람을 찾으면 -> 임마가 거리두기를 잘 지키나 확인
                     if (arr[r][c] == 'P') {
-                        boolean is_detected = detector(r, c, arr);
+                        boolean is_detected = my_detector(r, c, arr);
+                        // 2-2. 방역수칙 안지켜서 적발되면
                         if (is_detected) {
                             flag = true;
                             break;
                         }
                     }
-                    // 2-1. 방역수칙 어긴놈 적발시 -> 탐색종료
+                    // 2-3. 방역수칙 적발이미 되었으니 더 볼필요 없다
                     if (flag) break;
                 }
-                // 2-2. 방역수칙 어긴놈 적발시 -> 탐색 종료
+                // 2-4. 마찬가지로 방역수칙 위반자 이미 적발되었으니 더 볼필요 없음
                 if (flag) break;
             }
-            // 결과
-            if (flag) answer[t] = 0;
-            else if (!flag) answer[t] = 1;
+            
+            // 3. 판별
+            if (flag) {
+                answer[t] = 0;
+            }
+            else if (!flag) {
+                answer[t] = 1;
+            }
+            
         }
         return answer;
     }
     
     
-    
-    // 3. 함수 (탐색)
-    static boolean detector(int or, int oc, char[][] arr) {
-        // 3-1. 동서남북
+    // 4. 방역수칙 조사원
+    static boolean my_detector(int or, int oc, char[][] arr) {
+        // 4-1. 상하좌우
         boolean is_partition;
         for (int d = 0; d < 4; d++) {
             is_partition = false;
@@ -61,47 +69,48 @@ class Solution {
                 int nr = cr + dr[d];
                 int nc = cc + dc[d];
                 
-                // 범위 밖 -> 볼 필요없으니 break
+                // 4-1-a 범위 밖 -> skip
                 if (nr < 0 || nr >= row || nc < 0 || nc >= col) continue;
                 
-                // 범위 내
-                // 파티션이 있다면?
+                // 4-1-b 범위 내
+                // 만약에 파티션이 다음 좌표네?
                 if (arr[nr][nc] == 'X') {
                     is_partition = true;
                 }
-                // 사람이라면?
+                // 그렇지 않고, 다음좌표가 사람이야
                 else if (arr[nr][nc] == 'P') {
-                    // 파티션 없다면 
-                    // 방역수칙 어긴놈 검거
+                    // 그런데 파티션이 없다 -> 임마 바로 검거
                     if (!is_partition) {
                         return true;
                     }
                 }
-                // 좌표 갱신
+                // 4-1-c 좌표 갱신
                 cr = nr;
-                cc = nc;
+                cc = nc;  
+            }
+        }
+        
+        // 4-2. 대각선
+        for (int d = 0; d < 4; d++) {
+            int nr = or + dr_diag[d];
+            int nc = oc + dc_diag[d];
+            
+            // 4-2-a 범위 밖 -> skip
+            if (nr < 0 || nr >= row || nc < 0 || nc >= col) continue;
+            
+            // 4-2-b 범위 안
+            // 만약에 다음좌표갸 사람이라면?
+            if (arr[nr][nc] == 'P') {
+                // 두 곳중 한곳이라도 파티션이 없다면 --> 검거
+                if ( (arr[or][nc] != 'X') || (arr[nr][oc] != 'X') ) return true;
             }
         }
         
         
-        // 3-2. 대각선
-        for (int d = 0; d < dr_diag.length; d++) {
-            int nr = or + dr_diag[d];
-            int nc = oc + dc_diag[d];
-            
-            // 범위 밖 -> skip
-            if (nr < 0 || nr >= row || nc < 0 || nc >= col) continue;
-            
-            // 만약 다음 좌표가 사람이면
-            if (arr[nr][nc] == 'P') {
-                // 양쪽이 파티션이 아닐때 -> 거리두기수칙 위반
-                if (arr[nr][oc] != 'X' || arr[or][nc] != 'X') return true;    
-            }            
-        }
+        
+        // 4-3. 별일 없으면 정상으로 판별
         return false;
     }
-            
-
     
     
 }
