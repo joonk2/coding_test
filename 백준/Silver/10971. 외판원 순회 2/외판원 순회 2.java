8b8ff6789f8830d64283f0ec69f2a368289eb0b2
joonhwan.K <with.joonk@gmail.com>
2026-03-18 23:01:42 +0900

    [Silver II] Title: 외판원 순회 2, Time: 64 ms, Memory: 11760 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
+
+// test
 import java.util.Arrays;
 
 public class Main {
@@ -10,6 +12,7 @@ public class Main {
 	static int[][] DP;
 	static int INF = Integer.MAX_VALUE;
 	static int min_cost = Integer.MAX_VALUE;
+	
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,8 +29,9 @@ public class Main {
 			Arrays.fill(DP[r], INF);
 		}
 		
-		// 0번 도시에서 시작
-		// 시작도시 방문했다는 표시로 mask=1
+		
+		// 0번 도시에서 방문을 시작한다고 가정하자
+		// 시작도시 방문했다는 표시로 mask = 1
 		int start = 0;
 		dfs(start, 0, 1, 0);
 		System.out.println(min_cost);
@@ -35,16 +39,14 @@ public class Main {
 	
 	
 	
-	
 	static void dfs(int start, int cur, int mask, int cost) {
-		// 모든 도시를다 방문했다면
+		// 모든도시를 다 방문했다면
 		if (mask == (1<<N)-1) {
 			if (W[cur][start] != 0) {
 				min_cost = Math.min(min_cost, cost + W[cur][start]);
 			}
 			return;
 		}
-		
 		
 		// 가지치기
 		if (cost >= DP[cur][mask]) return;
@@ -53,20 +55,17 @@ public class Main {
 		DP[cur][mask] = cost;
 		
 		for (int next = 0; next < N; next++) {
-			// 다음 도시를 방문 안했고, 도시가 다르다면
-			if ( (mask & (1<<next)) == 0 && W[cur][next] != 0 ) {
+			// 다음 도시를 방문 안했고, 도시가 다르다면?
+			if ( (mask & (1<<next)) == 0  && W[cur][next] != 0) {
 				int next_cost = cost + W[cur][next];
 				
-				// 비용이 작다면 dfs 진행
+				// 비용이 더 싸다면 dfs 진행
 				if (next_cost < min_cost) {
 					dfs(start, next, mask | (1<<next), next_cost);
 				}
 			}
 		}
-		
 	}
-	
-	
 	
 	
 }
