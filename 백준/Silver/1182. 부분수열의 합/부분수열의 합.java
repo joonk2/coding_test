87ac7f3efa0d65c59b551607948a1820cecf53a9
joonhwan.K <with.joonk@gmail.com>
2026-03-18 14:40:23 +0900

    [Silver II] Title: 부분수열의 합, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,69 +1,52 @@
-/*
-[시도 방법]
-1. 연속적인 부분수열합
-2. 비연속적인 부분수열 합 --> 백트랙킹
-
-[틀린 이유]
-1. 연속적인 부분수열만 고려했음
-반례 -->
-5 0
--7 -3 -2 8 5
-
-*/
-
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int N, S, sum, cnt;
-	static int[] lst;
+	static int N, S;
+	static int cnt = 0;
+	static int[] arr;
 	
-	public static void main(String[] arsg) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] arr = br.readLine().split(" ");
-		N = Integer.parseInt(arr[0]);
-		S = Integer.parseInt(arr[1]);
+		String[] NS = br.readLine().split(" ");
+		String[] s_arr = br.readLine().split(" ");
+		N = Integer.parseInt(NS[0]);
+		S = Integer.parseInt(NS[1]);
+		arr = new int[N];
 		
-		String[] sequence = br.readLine().split(" "); 
-		
-		lst = new int[N];
 		for (int i = 0; i < N; i++) {
-			lst[i] = Integer.parseInt(sequence[i]);
+			arr[i] = Integer.parseInt(s_arr[i]);
 		}
 		
-        
-        // backtrack
-		cnt = 0;
-		backtrack(0,0, false);
+		// 1. 초기 탐색 시작
+		for (int i = 0; i < N; i++) {
+			back_tracking(i+1, arr[i]);
+		}
+		
+		// 3. 출력
 		System.out.println(cnt);
-		
-		
-		
-		
-		
-		
 	}
 	
 	
-	public static void backtrack(int i, int sum, boolean isSelected) {
-        // 원소 5개까지 포함하여 다 돌아봤을 때
-		if (i == N) {
-			if (sum == S && isSelected == true) {
-				cnt++;
-			}
+
+	// 2. 조합 (부분집합의 합을 구하기 위함)
+	static void back_tracking(int depth, int sum) {
+		
+		// 2-1. 최대깊이 도달시
+		if (depth == N) {
+			if (sum == S) cnt++;
 			return;
 		}
 		
+		// 2-2. 아직 도달 못했을때
 		
-		// 1. 현재 원소를 선택할 때
-		backtrack(i+1, sum + lst[i], true);
+		// a. 선택
+		back_tracking(depth + 1, sum + arr[depth]);
 		
-		// 2. 현재 원소를 선택 안할 때
-		backtrack(i+1, sum, isSelected);
-		
+		// b. 미선택
+		back_tracking(depth + 1, sum);
 	}
-	
 	
 	
 }
