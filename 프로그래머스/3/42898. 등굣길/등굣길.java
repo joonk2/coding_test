0cb90e83cd3a88cc1bce7455b49b3a8339df634d
joonhwan.K <with.joonk@gmail.com>
2026-04-20 17:38:25 +0900

    [level 3] Title: 등굣길, Time: 0.76 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,56 +1,58 @@
 class Solution {
-    static int row, col;
-    static int[][] DP;
-    static int er, ec;
     static int mod = 1000000007;
     
+    
     public int solution(int m, int n, int[][] puddles) {
-        row = n;
-        col = m;
-        DP = new int[row][col];
+        int row = n;
+        int col = m;
+        int[][] DP = new int[row][col];
         
-        // 1. 초기값
-        DP[0][0] = 1;
-        er = row-1;
-        ec = col-1;
-        
-        // 2. puddles 채우기
-        for (int[] pu : puddles) {
-            int r = pu[1]-1;
-            int c = pu[0]-1;
-            DP[r][c] = -1;
+        // 1. 웅덩이를 채우자
+        for (int[] i : puddles) {
+            int cur_r = i[1]-1;
+            int cur_c = i[0]-1;
+            DP[cur_r][cur_c] = -1;
         }
         
-        // 2. 우측 테두리 채우기
+        // 2  테두리 채우기
+        
+        // 2-1. 시작점 무조건 [0][0]
+        DP[0][0] = 1;
+        
+        // 2-2. 행 테두리 채우기 (왼 -> 오)
         for (int c = 1; c < col; c++) {
-            if (DP[0][c] == -1) continue;
+            // 현재값이 웅덩이라면 그만 봐라
+            if (DP[0][c] == -1) break;
+            
+            // 현재값이 웅덩이가 아니라면 계속 이전값을 갱신
             DP[0][c] = DP[0][c-1];
         }
         
-        // 3. 아래 테두리 채우기
+        // 2-3. 열 테두리 채우기 (위 -> 아래)
         for (int r = 1; r < row; r++) {
-            if (DP[r][0] == -1) continue;
+            // 현재값이 웅덩이라면 그만 봐라
+            if (DP[r][0] == -1) break;
+            
+            // 현재값이 웅덩이가 아니라면 계속 이전값을 갱신해라
             DP[r][0] = DP[r-1][0];
         }
         
-        // 4. 탐색
+        // 3. 채우기
         for (int r = 1; r < row; r++) {
             for (int c = 1; c < col; c++) {
-                // 4-1. 연못이면 skip
+                // 3-1. 웅덩이라면 넘어가자
                 if (DP[r][c] == -1) continue;
-                // 4-2. 그렇지 않다면
-                else if (DP[r][c] != -1) {
-                    int up = DP[r-1][c];
-                    int left = DP[r][c-1];
-                    if (DP[r-1][c] == -1) up = 0;
-                    if (DP[r][c-1] == -1) left = 0;
-                    
-                    DP[r][c] = (up + left) % mod;
-                }
+                
+                // 3-2 웅덩이가 아니라면
+                int up = DP[r-1][c];
+                int left = DP[r][c-1];
+                if (up == -1) up = 0;
+                if (left == -1) left = 0;
+                DP[r][c] = (up + left) % mod; 
             }
         }
         
         
-        return DP[er][ec];
+        return DP[row-1][col-1];
     }
 }
