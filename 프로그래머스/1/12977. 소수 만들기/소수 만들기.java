76620dabf2c114d3b6ca96c843e230e6d304558b
joonhwan.K <with.joonk@gmail.com>
2026-04-28 22:49:18 +0900

    [level 1] Title: 소수 만들기, Time: 84.75 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -9,7 +9,6 @@ class Solution {
         return answer;
     }
     
-    
     static void back_tracking(int idx, int cnt, int sum, int[] nums) {
         // 1. cnt 도달시 계산
         if (cnt == 3) {
@@ -19,9 +18,8 @@ class Solution {
             return;
         }
         
-        // 2. 가지치기 -> idx 초과 or (현재 고른횟수와 남은걸 다 골라도 3보다 작을때)
+        // 2. 가지치기 -> idx 초과 or (현재 고른횟수와 남은걸 다 골라도 3개보다 안된다면)
         if (idx >= N || cnt + (N-idx) < 3) return;
-        
         
         // 3-1. 선택
         back_tracking(idx+1, cnt+1, sum + nums[idx], nums);
@@ -32,16 +30,16 @@ class Solution {
     
     
     
+    // 4. 소수인가?
     static boolean is_prime(int sum) {
-        int res = 0;
-        for (int i = 1; i < sum+1; i++) {
-            if (sum % i == 0) res++;
+        int cnt = 0;
+        
+        for (int i = 1; i < sum + 1; i++) {
+            if (sum % i == 0) cnt++;
         }
         
-        if (res == 2) return true;
-        
+        if (cnt == 2) return true;
         return false;
     }
-    
     
 }
