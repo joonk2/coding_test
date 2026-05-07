b745b05ba041fbf4b82ac34f63b059c0fbe60f6f
joonhwan.K <with.joonk@gmail.com>
2026-05-08 01:13:03 +0900

    [D2] Title: 나눗셈 게임, Time: 74 ms, Memory: 24,960 KB -BaekjoonHub

---
+++ SWEA/D2/26409. 나눗셈 게임/나눗셈 게임.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+
+public class Solution {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc < T+1; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			String[] s = br.readLine().split(" ");
+			int[] num = new int[s.length];
+			for (int r = 0; r < N; r++) {
+				num[r] = Integer.parseInt(s[r]);
+			}
+			
+			// 1. 배열 생성
+			int sum = 0;
+			int[][] arr = new int[N][N];
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					if (r == c) continue;
+					
+					int temp_val = num[r] % num[c];
+					arr[r][c] = temp_val;
+					sum += arr[r][c];
+				}
+			}
+			
+			
+			// 2. 출력
+			System.out.println("#" + tc + " " + sum);
+			
+			
+		}
+		
+		
+	}
+}
