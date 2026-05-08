776e5507d969be7454a6419f7efb7c4ef2f12b93
joonhwan.K <with.joonk@gmail.com>
2026-05-08 23:13:02 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,3 +1,11 @@
+# select USER_ID, PRODUCT_ID
+# from ONLINE_SALE
+# group by USER_ID, PRODUCT_ID
+# having count(*) >= 2
+# order by USER_ID asc, PRODUCT_ID desc
+
+
+
 select USER_ID, PRODUCT_ID
 from ONLINE_SALE
 group by USER_ID, PRODUCT_ID
