8fde6e729b2c582b78ce32908901f4df5398a6e3
joonhwan.K <with.joonk@gmail.com>
2026-05-15 22:56:48 +0900

    [D3] Title: 전봇대, Time: 261 ms, Memory: 42,912 KB -BaekjoonHub

--- SWEA/D3/10580. 전봇대/전봇대.java
+++ SWEA/D3/10580. 전봇대/전봇대.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-
 public class Solution {	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,8 +11,8 @@ public class Solution {
 			int N = Integer.parseInt(br.readLine());
 			
 			// 1. 배열 채우기
-			int[] A = new int[10001];
-			int[] B = new int[10001];
+			int[] A = new int[1001];
+			int[] B = new int[1001];
 			for (int i = 0; i < N; i++) {
 				String[] ai_bi = br.readLine().split(" ");
 				int ai = Integer.parseInt(ai_bi[0]);
@@ -26,14 +25,15 @@ public class Solution {
 			int cross_cnt = 0;
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					// 2-1. 본인이면 pass
+					// 2-1. 한 점을 기준으로 잡아야하니까 -> 본인과 달라야한다
 					if (i == j) continue;
 					
-					// 2-2. 왼쪽점은 asc, 우측점은 desc
-					if (A[i] < A[j] && B[i] > B[j]) cross_cnt++;
+					// 2-2. 다른점일때
+					if (A[i] < A[j] && B[i] > B[j]) cross_cnt++;				
 				}
 			}
 			
+			// 3. 출력
 			System.out.println("#" + tc + " " + cross_cnt);
 			
 		}
