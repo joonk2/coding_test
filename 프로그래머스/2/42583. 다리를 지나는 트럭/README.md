d95446b9a32a4e945f5f1558a7c45b5ccfdb270c
joonhwan.K <with.joonk@gmail.com>
2026-06-10 20:02:18 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 7.13 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 92.1 MB, 시간: 12.24 ms
+메모리: 73.5 MB, 시간: 7.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 28일 02:12:44
+2026년 06월 10일 20:02:02
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,37 +1,54 @@
-import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-
-        int time_ticktok = 0;
-        int i = 0;
-        int temp_sum = 0;
         
-        Queue<int[]> q = new ArrayDeque<>();
+        // 1. q 생성 (시간, 무게)
+        Queue<int[]> q = new LinkedList<>();
         
-        while (!q.isEmpty() || i < truck_weights.length) {
-            time_ticktok++;
-            
-            // 1. 다리 검사 먼저 진행 (트럭을 올려보낼 수 있는지)
+        // 2. 계산
+        int time = 1;
+        int N = truck_weights.length;
+        int idx = 0;
+        int limit = 0;
+        while (true) {
+            // 2-1. 내릴 수 있는지?
             if (!q.isEmpty()) {
-                int[] front = q.peek();
-                if (time_ticktok - front[0] == bridge_length) {
-                    temp_sum -= front[1];
-                    q.poll();
+                int[] cur_pos = q.peek();
+                int first_t = cur_pos[0];
+                int diff = time - first_t;
+                // 2-1-a. 만약 diff == 다라길이 라면
+                if (diff == bridge_length) {
+                    // 2-1-b. 적발
+                    int[] caught_pos = q.poll();
+                    int caught_w = caught_pos[1];
+                    limit -= caught_w;
                 }
             }
             
-            // 2. 트럭을 출발시킬 수 있는지 확인
-            if (i < truck_weights.length) {
-                if (temp_sum + truck_weights[i] <= weight) {
-                    temp_sum += truck_weights[i];
-                    q.add(new int[] {time_ticktok, truck_weights[i]});
-                    i++;
-                }
+            // 2-2. q가 비었고 더이상 다리에 올릴 트럭이 없다면 종료
+            if (q.isEmpty() && idx >= N) break;
+            
+            // 2-3. 인덱스 초과라면
+            if (idx >= N) {
+                time++;
+                continue;
             }
+            
+            // 2-4. 트럭이 다리에 올라올 수 있다면?
+            int cur_w = truck_weights[idx];
+            if (weight >= limit + cur_w) {
+                limit += cur_w;
+                q.add(new int[] {time, cur_w});
+                idx++;
+            }
+            
+            // 3. 시간 추가
+            time++;
             
         }
-        return time_ticktok;
+        
+        return time;
     }
 }
