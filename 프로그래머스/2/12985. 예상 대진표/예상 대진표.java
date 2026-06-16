0e80d7330870897be84fc39f00c5dc64139d9066
joonhwan.K <with.joonk@gmail.com>
2026-06-17 05:57:07 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,14 +1,15 @@
 class Solution {
     public int solution(int n, int a, int b) {
-        int cnt = 0;
+        int answer = 0;
         
         while (true) {
             a = (a+1) / 2;
             b = (b+1) / 2;
-            cnt++;
+            answer++;
+            
             if (a == b) break;
         }
-        
-        return cnt;
+
+        return answer;
     }
 }
