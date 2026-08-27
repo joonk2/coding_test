60889c7960ab33032fea3c76b0f2d5dc22ee20ef
joonhwan.K <with.joonk@gmail.com>
2026-08-27 22:25:13 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 9.24 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -3,53 +3,63 @@ class Solution {
     
     public int[] solution(int[][] arr) {
         answer = new int[2];
+        
+        // 1. quad_tree
         int sr = 0;
         int sc = 0;
         int N = arr.length;
+        quad_tree(sr, sc, arr, N);
         
-        back_tracking(sr, sc, N, arr);
+        // 2. 결과값
         return answer;
     }
     
     
     
-    static void back_tracking(int r, int c, int cur_size, int[][] arr) {
-        // 1. 첫 원소
-        int first_val = arr[r][c];
+    // 3. quad_tree 함수
+    static void quad_tree(int cr, int cc, int[][] arr, int N) {
         
-        // 1. 검사
-        if (is_all_same(first_val, r, c, cur_size, arr)) {
-            answer[first_val]++;
+        // 3-1-a. 격자 안의 값이 첫 값과 다 같은지 확인
+        int point = arr[cr][cc];
+        boolean is_all_same = true;
+        for (int r = cr; r < cr + N; r++) {
+            for (int c = cc; c < cc + N; c++) {
+                // 3-1-b. 1개라도 틀리면 종료
+                if (arr[r][c] != point) {
+                    is_all_same = false;
+                    break;
+                }
+            }
+            // 3-1-c. 이미 1개라도 틀렸으면 break
+            if (!is_all_same) break;
+        }
+        
+        // 3-1-d. 만약 전부 같으면 -> 종료
+        if (is_all_same) {
+            answer[point]++;
             return;
         }
         
-        // 2. 4방향 탐색
-        int new_size = cur_size / 2;
         
-        // 2-1 왼위
-        back_tracking(r, c, new_size, arr);
+        // 3-2. 1개라도 틀려서 압축 못했을시
+        int nr = cr;
+        int nc = cc;
+        int half = N / 2;
         
-        // 2-2. 오위
-        back_tracking(r, c+new_size, new_size, arr);
+        // 3-3. 재귀
         
-        // 2-3. 왼아래
-        back_tracking(r+new_size, c, new_size, arr);
+        // 3-3-a upper_left
+        quad_tree(nr, nc, arr, half);
         
-        // 2-4 오아래
-        back_tracking(r+new_size, c+new_size, new_size, arr);
+        // 3-3-b. upper_right
+        quad_tree(nr, nc + half, arr, half);
+        
+        // 3-3-c. down_left
+        quad_tree(nr + half, nc, arr, half);
+        
+        // 3-3-d. down_right
+        quad_tree(nr + half, nc + half, arr, half);
     }
-    
-    
-    static boolean is_all_same(int val, int r, int c, int cur_size, int[][] arr) {
-        for (int i = r; i < r + cur_size; i++) {
-            for (int j = c; j < c + cur_size; j++) {
-                if (arr[i][j] != val) return false;
-            }
-        }
-        
-        return true;
-    }
-    
     
     
 }
